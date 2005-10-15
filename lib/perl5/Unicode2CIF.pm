017ff22861f47065a10685989bf3129083679b81
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2005-10-15 18:03:34 +0000

    Forst working version that transliterates Greek, accented characters and some of CIF mathematical and chemical symbols (but not all of them). Also, it does not ensure a round-trip of generic Unicode characters.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@25 04be6746-3802-0410-999d-98508da1e98c

--- lib/perl5/Unicode2CIF.pm
+++ lib/perl5/Unicode2CIF.pm
@@ -8,18 +8,70 @@
 package Unicode2CIF;
 
 use strict;
+use Unicode::Normalize;
 ## use utf8;
-use charnames ':full';
+## use charnames ':full';
 
 require Exporter;
 @ShowStruct::ISA = qw(Exporter);
 @ShowStruct::EXPORT = qw(unicode2cif cif2unicode);
 
 my %cif = (
-    "\N{RIGHTWARDS ARROW}" => '\\\\rightarrow ', # RIGHTWARDS ARROW
-    "\N{LEFTWARDS ARROW}"  => '\\\\leftarrow ', # LEFTWARDS ARROW
-    "\N{UP TACK}"          => '\\\\x{22A5}', # UP TACK, similar to PERPENDICULAR
-    "\x{27C2}"             => '\\\\x{27C2}', # PERPENDICULAR
+#
+# Arrows and mathematical symbols:
+#
+    "\x{2190}" => '\\\\leftarrow ',  # LEFTWARDS ARROW
+    "\x{2192}" => '\\\\rightarrow ', # RIGHTWARDS ARROW
+    "\x{22A5}" => '\\x{22A5}',       # UP TACK, similar to PERPENDICULAR
+    "\x{27C2}" => '\\x{27C2}',       # PERPENDICULAR
+
+    "\x{00B0}" => '\%',       # DEGREE SIGN
+    "\x{00D7}" => '\\times ', # MULTIPLICATION SIGN
+
+# --
+# dash
+
+# 
+# +-
+# ±
+# 
+# ---
+# single bond
+# 
+# -+
+# 
+# \\db
+# double bond
+# 
+# \\square
+# square
+# 
+# \\tb
+# triple bond
+# 
+# \\neq
+# 
+# \\ddb
+# delocalized double bond
+# 
+# \\rangle
+# 
+# \\sim
+# 
+# \\langle
+# 
+# (N.B. ~  is the code for subscript)
+# \\rightarrow
+# 
+# \\simeq
+# 
+# \\leftarrow
+# 
+# \\infty
+
+#
+# Greek letters:
+#
     "\x{03B1}" => '\a', # alpha
     "\x{03B2}" => '\b', # beta
     "\x{03B3}" => '\g', # gamma
@@ -47,6 +99,28 @@ my %cif = (
     "\x{03C9}" => '\w', # omega
 );
 
+my %combining = (
+#
+# Combining diacritical marks:
+#
+   "\x{0300}" => '\\`',  #   COMBINING GRAVE ACCENT (grave)
+   "\x{0301}" => '\\\'', #'# COMBINING ACUTE ACCENT (acute)
+   "\x{0308}" => '\"',   #   COMBINING DIARESIS     (umlaut)
+   "\x{0304}" => '\=',   #   COMBINING MACRON       (overbar)
+   "\x{0303}" => '\~',   #   COMBINING TILDE        (tilde)
+   "\x{0307}" => '\.',   #   COMBINING DOT ABOVE    (overdot)
+   "\x{0302}" => '\^',   #   COMBINING CIRCUMFLEX ACCENT (circumflex)
+   "\x{0328}" => '\;',   #   COMBINING OGONEK       (ogonek)
+   "\x{030C}" => '\<',   #   COMBINING CARON        (hacek)
+   "\x{0327}" => '\,',   #   COMBINING CEDILLA      (cedilla)
+   "\x{030B}" => '\>',   #   COMBINING DOUBLE ACUTE ACCENT (Hungarian umlaut)
+   "\x{0306}" => '\(',   #   COMBINING BREVE (breve)
+); 
+
+#
+# Add upper-case Greek letters:
+#
+
 for my $i ( 0x0391 .. 0x03A9 ) {
     if( $i != 0x03A2 ) { # reserved code-point, could be an uppercase varsigma
 	my $c = chr($i);
@@ -56,12 +130,43 @@ for my $i ( 0x0391 .. 0x03A9 ) {
     }
 }
 
+my %utf = ();
+
+while( my ($key,$val) = each %cif ) {
+    $utf{$val} = $key;
+}
+
 sub unicode2cif
 {
-    my $text = $_[0];
+    my $text = Unicode::Normalize::normalize( 'D', $_[0] );
 
     for my $pattern (keys %cif) {
 	$text =~ s/$pattern/$cif{$pattern}/g;
+    }
+    for my $pattern (keys %combining) {
+	$text =~ s/(.)($pattern)/$2$1/g;
+	$text =~ s/$pattern/$combining{$pattern}/g;
+    }
+    return $text;
+}
+
+sub cif2unicode
+{
+    my $text = $_[0];
+
+    for my $pattern (keys %combining) {
+	$text =~ s/(\Q$combining{$pattern}\E)(.)/$2$1/g;
+	$text =~ s/\Q$combining{$pattern}\E/$pattern/g;
+    }
+    for my $pattern (keys %utf) {
+    	$text =~ s/\Q$pattern/$utf{$pattern}/g;
+    	if( $pattern =~ /\s$/ ) {
+    	    my $core = $pattern;
+    	    $core =~ s/\s$//;
+    	    $text =~ s/\Q$core\E([^a-zA-Z0-9])/$utf{$pattern}$1/g;
+    	    $text =~ s/\Q$core\E([^a-zA-Z0-9])/$utf{$pattern}$1/g;
+    	    $text =~ s/\Q$core\E$/$utf{$pattern}/g;
+    	}
     }
     return $text;
 }
