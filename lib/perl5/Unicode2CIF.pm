4da1d0719ecbc05f8d6bc26de862d79edbb203a4
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2005-10-16 10:00:36 +0000

    Syntax of the hexadecimal character entities corrected to match that of the WWW specification.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@29 04be6746-3802-0410-999d-98508da1e98c

--- lib/perl5/Unicode2CIF.pm
+++ lib/perl5/Unicode2CIF.pm
@@ -22,8 +22,8 @@ my %cif = (
 #
     "\x{2190}" => '\\\\leftarrow ',  # LEFTWARDS ARROW
     "\x{2192}" => '\\\\rightarrow ', # RIGHTWARDS ARROW
-    "\x{22A5}" => '&0x22A5;',        # UP TACK, similar to PERPENDICULAR
-    "\x{27C2}" => '&0x27C2;',        # PERPENDICULAR
+    "\x{22A5}" => '&#x22A5;',        # UP TACK, similar to PERPENDICULAR
+    "\x{27C2}" => '&#x27C2;',        # PERPENDICULAR
     "\x{00D7}" => '\\\\times ',      # MULTIPLICATION SIGN (times)
     "\x{2012}" => '--',              # EN DASH             (dash) (?)
     "\x{2013}" => '--',              # EN DASH             (dash)
