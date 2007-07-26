1fc710a6a6787a34bd41cf45c84fa7fd309c8ae3
justas <justas@04be6746-3802-0410-999d-98508da1e98c>
2007-07-26 07:35:01 +0000

    Updated CIFParser - added version number variable to it, for reuse in other modules, or info message if it will be present in the future.
    Updated CIFParser dictionary check module:
    	- Perl core module Getopt::Long is used to parse arguments passed in 
    	command line;
    	- comments are added to code to separate parts;
    	- perl 'shebang' is added;
    	- created subroutines VersionMessage and HelpMessage (text inside 
    	them should be created or updated).
    
    
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@207 04be6746-3802-0410-999d-98508da1e98c

--- CIFParser/cif_check_dict.pm
+++ CIFParser/cif_check_dict.pm
@@ -1,20 +1,72 @@
+#! /usr/bin/perl -w
+
+#
+# here we link libraries
+#
 use strict;
 use warnings;
-use SOptions;
+#use SOptions;
+use Getopt::Long;
 use ShowStruct;
-use CIFParse;
+use CIFParser;
+
+#
+# here we define variables
+#	first - constant values
+#	then  - common variables
+#
+my $version = 0.1;
 
 my @input_file;
 my $output_file;
 my $dict_file;
 my $quiet = 0;
-my $parser = new CIFParse;
+my $parser = new CIFParser;
 
-@input_file = SOptions::getOptions ({
-	"-o, --output" => \$output_file,
-	"-D, --dictionary" => \$dict_file,
-	"-v, --version" => showVersion;
-	"-q, --quiet" => sub{ $quiet = 1; },
-	"--no-quiet" => sub{ $quiet = 0; } }, @ARGV);
+#
+# here we declare subroutines
+#
 
-showRef(@input_file);
+# subroutine to print module version and copyright notice
+sub VersionMessage;
+
+# subroutine to print help on module usage
+sub HelpMessage;
+
+
+#
+# main program code
+#
+
+@input_file = Getopt::Long::GetOptions
+	("output"			=> \$output_file,
+		"dictionary"	=> \$dict_file,
+		"version"		=> sub { VersionMessage() },
+		"quiet"			=> sub { $quiet = 1; },
+		"no-quiet"		=> sub { $quiet = 0; },
+		"help|?"		=> sub { HelpMessage() });
+
+#
+# here goes all subroutines bodies
+#
+
+sub VersionMessage
+{
+	print <<END_M;
+cif_check_dict (using CIFParser v.$CIFParser::version) v.$version
+Copyright (C) 2007 ???
+This is free software. You may redistribute copies of it under the terms of
+the GNU General Public License <http://www.gnu.org/licenses/gpl.html>.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by Justas Butkus.
+END_M
+}
+
+sub HelpMessage
+{
+	print <<END_M;
+I hope you know, what you are doing.
+If not - feel recommended to wait until release of production version.
+END_M
+}
