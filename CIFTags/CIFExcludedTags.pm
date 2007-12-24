87e91ca6bc44d9ce3c3ca54e2d7af4a8e0fb0cfb
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2007-12-24 21:09:04 +0000

    Adding tags _publ_section_exptl_refinement, _exptl_special_details, and _geom_special_details into the list of potentially copyrighted tags.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@305 04be6746-3802-0410-999d-98508da1e98c

--- CIFTags/CIFExcludedTags.pm
+++ CIFTags/CIFExcludedTags.pm
@@ -167,6 +167,10 @@ _publ.section_synopsis
 _publ_section_table_legends
 _publ.section_table_legends
 
+_publ_section_exptl_refinement
+_exptl_special_details
+_geom_special_details
+
 _publ_vrn_code
 );
 
