2f30aeb29e98dfb25c52290ec29d373268813846
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2008-01-02 15:13:24 +0000

    Copied the trunk/CIFParser directory into the trunk/CIFParser-fight-for-speed/, to tinker with various tokenisation methods of a CIF file and in a desperate searche for faster CIF parsing algorithm in Perl.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@325 04be6746-3802-0410-999d-98508da1e98c

---
+++ CIFParser-fight-for-speed/shtests/loop_001.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+#!perl -w # --*- Perl -*--
+eval 'exec perl -x $0 ${1+"$@"}'
+    if 0;
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#  Perl test driver.
+#**
+
+use strict;
+
+use lib ".";
+use lib "lib/perl5";
+use lib "CIFParser";
+
+use File::Basename;
+use CIFParser;
+
+my $script_dir  = File::Basename::dirname( $0 );
+my $script_name = File::Basename::basename( $0 );
+
+$script_name =~ s/\.sh$//;
+
+my $filename = "${script_dir}/${script_name}.inp";
+
+my $parser = new CIFParser;
+
+my $data = $parser->Run($filename);
+
+local $, = ", ";
+local $\ = "\n";
+
+for my $loop (@{$data->[0]{loops}}) {
+    print @$loop;
+}
