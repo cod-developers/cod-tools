5b9ff92f994e3d31c38b748cf212d02cf59261af
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2008-01-19 14:34:31 +0000

    Adding a Perl module for CIF tag management (CIFTags/CIFTagManage.pm) into trunk/, and adding the --exclude-empty-tags option into the cif_filter script that uses that module.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@339 04be6746-3802-0410-999d-98508da1e98c

---
+++ CIFTags/CIFTagManage.pm
@@ -0,0 +1,68 @@
+#------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------
+#* 
+#  CIF tag management functions that work on the internal
+#  representation of a CIF file returned by the CIFParser module.
+#**
+
+package CIFTagManage;
+
+use strict;
+
+require Exporter;
+@CIFTagManage::ISA = qw(Exporter);
+@CIFTagManage::EXPORT = qw( exclude_tag tag_is_empty exclude_empty_tags );
+
+sub exclude_tag
+{
+    my ($cif, $tag) = @_;
+
+    undef( $cif->{values}{$tag} );
+    undef( $cif->{precisions}{$tag} );
+    @{$cif->{tags}} =
+	grep ! /^\Q$tag\E$/, @{$cif->{tags}};
+
+    if( $cif->{inloop}{$tag} ) {
+	my $loop_nr = $cif->{inloop}{$tag};
+	undef $cif->{inloop}{$tag};
+	@{$cif->{loops}[$loop_nr]} =
+	    grep ! /^\Q$tag\E$/, @{$cif->{loops}[$loop_nr]};
+    }
+}
+
+sub tag_is_empty
+{
+    my ($cif, $tag) = @_;
+    my $is_empty =1;
+
+    if( exists $cif->{values}{$tag} ) {
+	for my $val (@{$cif->{values}{$tag}}) {
+	    if( defined $val && $val ne "?" && $val ne "." ) {
+		$is_empty = 0;
+		last;
+	    }
+	}
+    }
+    return $is_empty;
+}
+
+sub exclude_empty_tags
+{
+    my $cif = $_[0];
+    my @empty_tags = ();
+
+    for my $tag (@{$cif->{tags}}) {
+	if( tag_is_empty( $cif, $tag )) {
+	    push( @empty_tags, $tag );
+	}
+    }
+    for my $empty_tag (@empty_tags) {
+	exclude_tag( $cif, $empty_tag );
+    }
+}
+
+1;
