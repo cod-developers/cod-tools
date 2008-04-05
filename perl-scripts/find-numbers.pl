0a121d607b5646dced46b3c88bd52e6f706125c1
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2008-04-05 10:03:54 +0000

    Adding the find-numbers.pl script from the xyz-extraction repository, to be further developed here.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@436 04be6746-3802-0410-999d-98508da1e98c

---
+++ perl-scripts/find-numbers.pl
@@ -0,0 +1,310 @@
+#! /bin/sh
+#!perl -w # --*- Perl -*--
+eval 'exec perl -x $0 ${1+"$@"}'
+    if 0;
+#------------------------------------------------------------------------------
+#$Author: saulius $
+#$Date: 2008-03-29 08:18:40 +0200 (Sat, 29 Mar 2008) $
+#$Revision: 536 $
+#$URL: svn+ssh://pitonas.ibt.lt/home/xray/svn-repositories/xyz-extraction/deposit/bin/find-numbers.pl $
+#------------------------------------------------------------------------------
+#*
+#  Find COD numbers for the .cif files in given directories of file lists.
+#
+#  Usage:
+#      find-numbers.pl my-cif-dir1/ my-cif-dir2/files*.cif  COD-cif-dir/ 
+#**
+
+use strict;
+use File::Basename;
+
+my $check_bibliography = 1;
+
+my $max_cell_length_diff = 0.5; # Angsroems
+my $max_cell_angle_diff  = 1.2; # degrees
+
+my %COD = ();
+
+if( @ARGV < 2 ) {
+    print STDERR "$0: please supply two directory names on the command line:\n";
+    print STDERR "first directory with the analysed CIF files,\nand the second ";
+    print STDERR "with the COD CIF files.\n";
+    exit -1;
+}
+
+my $COD_cif_dir = pop( @ARGV );
+my @COD_cif_files = `find $COD_cif_dir -name "*.cif" -o -name "*.CIF"`;
+
+do {
+    print int(@COD_cif_files), "\n";
+    print "@COD_cif_files";
+} if 0;
+
+for my $file (@COD_cif_files) {
+
+    chomp $file;
+    open( CODCIF, "<$file" ) or
+	die( "$0: could not open file '$file' for reading: $!" );
+
+    my %structures = ();
+    my $id;
+
+    while( <CODCIF> ) {
+	if( /^\s*data_(.*)/ ) {
+	    $id = $1;
+	}
+	if( /_chemical_formula_sum\s+(.*)/ ) {
+	    $structures{$id}{chemical_formula_sum} = $1;
+	    $structures{$id}{chemical_formula_sum} =~ s/^\s*'\s*|\s*'\s*$//g;
+	}
+	if( /(_cell_(?:length_a|length_b|length_c|
+                     angle_alpha|angle_beta|angle_gamma))\s+([\d.]+)/x ) {
+	    my $key = $1;
+	    my $val = $2;
+	    $val =~ s/^\s*'\s*|\s*'\s*$//g;
+	    $val =~ s/\(.*$//;
+	    $val =~ s/[()_a-zA-Z]//g;
+	    $structures{$id}{cell}{$key} = sprintf "%f", $val;
+	}
+	if( /(_journal_[^\s]*)\s+(.*)\s*$/ ) {
+	    my $key = $1;
+	    my $value = $2;
+	    $value =~ s/^['"]|["']$//g;
+	    $structures{$id}{bibliography}{$key} = $value;
+	}
+    }
+
+    close( CODCIF );
+
+    my $basename = File::Basename::basename( $file );
+    for my $id (keys %structures) {
+	my $cell = $structures{$id}{cell};
+	my $biblio = $structures{$id}{bibliography};
+	my $formula = $structures{$id}{chemical_formula_sum};
+
+	$formula = '?' unless defined $formula;
+
+	push( @{$COD{$formula}}, { id => $id,
+				   filename => "$basename",
+				   cell => $cell,
+				   bibliography => $biblio
+	                         } );
+    }
+}
+
+do {
+    use Serialise;
+    serialiseRef( \%COD );
+} if 0;
+
+#------------------------------------------------------------------------------
+
+my @cif_files = `find @ARGV -name "*.cif" -o -name "*.CIF"`;
+
+for my $file (@cif_files) {
+    chomp $file;
+    open( CIF, "<$file" ) or
+	die( "$0: could not open file '$file' for reading: $!" );
+
+    my %structures = ();
+    my $id;
+
+    while( <CIF> ) {
+	if( /^\s*data_(.*)/ ) {
+	    $id = $1;
+	    $structures{$id}{id} = $id;
+	    $structures{$id}{filename} = File::Basename::basename( $file );
+	}
+	if( /_chemical_formula_sum\s+(.*)/ ) {
+	    $structures{$id}{chemical_formula_sum} = $1;
+	    $structures{$id}{chemical_formula_sum} =~ s/^\s*'\s*|\s*'\s*$//g;
+	}
+	if( /(_cell_(?:length_a|length_b|length_c|
+                     angle_alpha|angle_beta|angle_gamma))\s+([\d.]+)/x ) {
+	    my $key = $1;
+	    my $val = $2;
+	    $val =~ s/^\s*'\s*|\s*'\s*$//g;
+	    $val =~ s/\(.*$//;
+	    $structures{$id}{cell}{$key} = sprintf "%f", $val;
+	}
+	if( /(_journal_[^\s]*)\s+(.*)\s*$/ ) {
+	    my $key = $1;
+	    my $value = $2;
+	    $value =~ s/^['"]|["']$//g;
+	    $structures{$id}{bibliography}{$key} = $value;
+	}
+    }
+
+    for $id (keys %structures) {
+	my $formula = $structures{$id}{chemical_formula_sum};
+
+	$formula = '?' unless defined $formula;
+
+	my $final_formula = $formula;
+	$final_formula =~ s/\s/_/g;
+
+	my $n = 0;
+	if( defined $COD{$formula} ) {
+	    for my $COD_entry (@{$COD{$formula}}) {
+		if( entries_are_the_same( $structures{$id}, $COD_entry )) {
+		    $n++;
+		}
+	    }
+	}
+	if( $n > 0 ) {
+	    for my $COD_entry (@{$COD{$formula}}) {
+		if( entries_are_the_same( $structures{$id}, $COD_entry )) {
+		    printf
+			"%-35s %15s %3d %s\n",
+			$final_formula, 
+			$COD_entry->{filename}, $n, $file;
+		}
+	    }
+	} else {
+	    printf "%-35s %15s %3d %s\n", $final_formula, "?", 0, $file;
+	}
+    }
+
+    close( CIF );
+}
+
+#------------------------------------------------------------------------------
+
+sub get_cell($)
+{
+    my $datablok = $_[0];
+
+    return (
+	$datablok->{_cell_length_a},
+	$datablok->{_cell_length_b},
+	$datablok->{_cell_length_c},
+	$datablok->{_cell_angle_alpha},
+	$datablok->{_cell_angle_beta},
+	$datablok->{_cell_angle_gamma}
+    );
+}
+
+sub cells_are_the_same($$)
+{
+    my ($cell1, $cell2) = @_;
+
+    my @cell1 = get_cell( $cell1 );
+    my @cell2 = get_cell( $cell2 );
+
+    my $max_length_diff = 0;
+    my $max_angle_diff = 0;
+
+    for my $i (0..2) {
+	my $length1 = $cell1[$i];
+	my $length2 = $cell2[$i];
+	if( defined $length1 and defined $length2 ) {
+	    my $diff = abs( $length1 - $length2 );
+	    if( $max_length_diff < $diff ) {
+		$max_length_diff = $diff;
+	    }
+	}
+    }
+    for my $i (3..5) {
+	my $angle1 = $cell1[$i];
+	my $angle2 = $cell2[$i];
+	if( defined $angle1 and defined $angle2 ) {
+	    my $diff = abs( $angle1 - $angle2 );
+	    if( $max_angle_diff < $diff ) {
+		$max_angle_diff = $diff;
+	    }
+	}
+    }
+
+    return
+	$max_length_diff < $max_cell_length_diff &&
+	$max_angle_diff < $max_cell_angle_diff;
+}
+
+my %has_numeric_value = (
+    "_journal_year"   => 1,
+    "_journal_volume" => 1,
+    "_journal_issue"  => 1,
+);
+
+my %skip_tag = (
+    "_journal_name_full" => 1,
+);
+
+sub bibliographies_are_the_same($$)
+{
+    my ($biblio1, $biblio2) = @_;
+
+    my %tags = map {($_,$_)} ( keys %$biblio1, keys %$biblio2 );
+
+    for my $tag ( keys %tags ) {
+	next if( $skip_tag{$tag} );
+	if( defined $biblio1->{$tag} && defined $biblio2->{$tag} ) {
+	    if( $has_numeric_value{$tag} ) {
+		if( $biblio1->{$tag} != $biblio2->{$tag} ) {
+		    return 0;
+		}
+	    } else {
+		if( $biblio1->{$tag} ne $biblio2->{$tag} ) {
+                    return 0;
+                }
+	    }
+	}
+    }
+    return 1;
+}
+
+sub data_sections_are_the_same($$)
+{
+    my ($entry1, $entry2) = @_;
+
+    return
+	$entry1->{filename} eq $entry2->{filename} &&
+	$entry1->{id} eq $entry2->{id};
+}
+
+sub entries_are_the_same
+{
+    my ($entry1, $entry2) = @_;
+
+    if( $check_bibliography ) {
+	return
+	    ! data_sections_are_the_same( $entry1, $entry2 ) &&
+	    cells_are_the_same( $entry1->{cell}, $entry2->{cell} ) &&
+	    bibliographies_are_the_same( $entry1->{bibliography},
+					 $entry2->{bibliography} );
+    } else {
+	return
+	    ! data_sections_are_the_same( $entry1, $entry2 ) &&
+	    cells_are_the_same( $entry1->{cell}, $entry2->{cell} );
+    }
+}
+
+## sub cell_volume
+## {
+##     my @cell = @_;
+## 
+##     if( !defined $cell[0] || !defined $cell[1] || !defined $cell[2] ||
+## 	!defined $cell[3] || !defined $cell[4] || !defined $cell[5] ) {
+## 	return -1;
+##     }
+## 
+##     my $Pi = 3.14159265358979;
+## 
+##     @cell = map { s/\(.*\)//g; $_ } @_;
+## 
+##     my ($a, $b, $c) = @cell[0..2];
+##     my ($alpha, $beta, $gamma) = map {$Pi * $_ / 180} @cell[3..5];
+##     my ($ca, $cb, $cg) = map {cos} ($alpha, $beta, $gamma);
+##     my $sg = sin($gamma);
+##     
+##     my $V = $a * $b * $c * sqrt( $sg**2 - $ca**2 - $cb**2 + 2*$ca*$cb*$cg );
+## 
+##     return $V;
+## }
+## 
+## sub compute_datablock_cell_volume
+## {
+##     my $values = $_[0];
+##     my @cell = get_cell( $values );
+##     return cell_volume( @cell );
+## }
