96819040a7cd70a5207b644e33a4d0052ab68bae
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2008-11-23 18:58:29 +0000

    Adding module CIFTagCanonicalNames.pm, which contains a subroutine, copied from the cif_filter script, that converts tags to "canonical" names (i.e. names with the predefined capitalisation). Adding a call to this function into the cif_cod_check script.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@659 04be6746-3802-0410-999d-98508da1e98c

---
+++ CIFTags/CIFTagCanonicalNames.pm
@@ -0,0 +1,63 @@
+#------------------------------------------------------------------------
+#$Author: saulius $
+#$Date: 2008-06-03 17:21:41 +0300 (Tue, 03 Jun 2008) $ 
+#$Revision: 499 $
+#$URL: svn+ssh://pitonas.ibt.lt/home/xray/svn-repositories/cif-tools/trunk/CIFTags/CIFTagManage.pm $
+#------------------------------------------------------------------------
+#* 
+#  CIF tag management functions that work on the internal
+#  representation of a CIF file returned by the CIFParser module.
+#**
+
+package CIFTagCanonicalNames;
+
+use strict;
+
+require Exporter;
+@CIFTagCanonicalNames::ISA = qw(Exporter);
+@CIFTagCanonicalNames::EXPORT = qw( canonicalize_names );
+
+use CIFDictTags;
+use CIFCODTags;
+
+my @dictionary_tags = ( @CIFDictTags::tag_list, @CIFCODTags::tag_list );
+
+my %cif_tags_lc = map {(lc($_),$_)} @dictionary_tags;
+
+sub canonicalize_names
+{
+    my ($cif) = @_;
+
+    # convert all tags to a "cannonical" form (the one used in this
+    # script ;):
+
+    for my $dataset (@{$cif}) {
+        my $datablok = $dataset->{values};
+        for my $key ( keys %{$datablok} ) {
+            my $lc_key = lc( $key );
+            ## print ">>> $key -> $lc_key\n";
+            if( defined $cif_tags_lc{$lc_key} ) {
+                my $cannonical_key = $cif_tags_lc{$lc_key};
+                ## print ">>> $key -> $lc_key -> $cannonical_key\n";
+                if( !exists $datablok->{$cannonical_key} ) {
+                    $datablok->{$cannonical_key} = $datablok->{$key};
+                    delete $datablok->{$key};
+                    @{$dataset->{tags}} =
+                        map { s/^$key$/$cannonical_key/; $_ }
+                    @{$dataset->{tags}};
+                    if( exists $dataset->{inloop}{$key} ) {
+                        my $loop_nr = $dataset->{inloop}{$key};
+                        $dataset->{inloop}{$cannonical_key} =
+                            $dataset->{inloop}{$key};
+                        delete $dataset->{inloop}{$key};
+                        @{$dataset->{loops}[$loop_nr]} =
+                            map { s/^$key$/$cannonical_key/; $_ }
+                        @{$dataset->{loops}[$loop_nr]};
+                    }
+                }
+            }
+        }
+    }
+}
+
+1;
