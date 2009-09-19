cce851d5221d138e658349676966ed1e268c8a08
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2009-09-19 07:48:19 +0000

    trunk/ Adding more precise test for ddata_global section into the cif_split_primitive script.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@934 04be6746-3802-0410-999d-98508da1e98c

---
+++ perl-scripts/shtests/cif_split_primitive_009.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -ue
+
+cif_split=./cif_split_primitive
+
+CIF=./inputs/2-entries-AMCSD-cifdata.cif
+
+BASENAME="`basename $0 .sh`"
+
+TMP_DIR="./tmp-${BASENAME}"
+
+mkdir ${TMP_DIR}
+
+cp ${CIF} ${TMP_DIR}
+cp ${cif_split} ${TMP_DIR}
+
+(
+    cd ${TMP_DIR}
+
+    CIF_BASE="`basename ${CIF}`"
+    CIF_CORE="`basename ${CIF} .cif`"
+
+    script_base="`basename ${cif_split}`"
+
+    mkdir ${CIF_CORE}
+
+    ./${script_base} --output-dir=${CIF_CORE}/ < ${CIF_BASE}
+
+    rm ${CIF_BASE}
+    rm ${script_base}
+)
+
+diff --exclude .svn -rs outputs/split/${BASENAME} ${TMP_DIR}
+
+rm -rf ${TMP_DIR}
