0526449ae489352350dab49d83f0bff7f98324d2
justas <justas@04be6746-3802-0410-999d-98508da1e98c>
2009-11-20 20:02:58 +0000

    trunk/ Changed handling of unquoted strings. Added option for CIFParser "allow_uqstring_brackets" which, if set to anything, that Perl treats as true (i.e. literal '1'), causes CIFParser to accept values like this '[myvalue', or '[mynewva]' (w/o quotes), otherwise it adheres to CIF syntax and values starting with opening bracket are treated as errors. Added many new tests, more than half-a-dozen from COD database to illustrate, that square bracket IS used as first symbol of unquoted string, although it is NOT valid. This is why option is introduced - otherwise we would have many problems with these CIFs.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@988 04be6746-3802-0410-999d-98508da1e98c

---
+++ CIFParser/shtests/unquoted_string.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+#!perl -w # --*- Perl -*--
+eval 'exec perl -x $0 ${1+"$@"}'
+    if 0;
+#------------------------------------------------------------------------------
+#$Author: saulius $
+#$Date: 2007-12-14 09:50:59 +0000 (Fri, 14 Dec 2007) $ 
+#$Revision: 250 $
+#$URL: svn+ssh://pitonas.ibt.lt/home/xray/svn-repositories/cif-tools/trunk/CIFParser/shtests/text_field_001.sh $
+#------------------------------------------------------------------------------
+#*
+#  Perl test driver.
+#**
+
+use strict;
+
+use lib ".";
+use lib "lib/perl5";
+use lib "CIFParser";
+
+use File::Basename;
+use CIFParser;
+
+my $script_dir  = File::Basename::dirname( $0 );
+my $script_name = File::Basename::basename( $0 );
+
+$script_name =~ s/\.sh$//;
+
+my $filename = "${script_dir}/${script_name}.inp";
+
+my $parser = new CIFParser;
+
+my $data = $parser->Run($filename);
+
+while(my($k,$v) = each(%{$data->[0]{values}})) {
+    print "Tag '" . $k . "' values:\n";
+    local $, = "\n";
+    print @{$v};
+    print "\n" . '-' x 20 . "\n";
+}
---
+++ CIFParser/shtests/unquoted_string_wo_brackets.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+#!perl -w # --*- Perl -*--
+eval 'exec perl -x $0 ${1+"$@"}'
+    if 0;
+#------------------------------------------------------------------------------
+#$Author: saulius $
+#$Date: 2007-12-14 09:50:59 +0000 (Fri, 14 Dec 2007) $ 
+#$Revision: 250 $
+#$URL: svn+ssh://pitonas.ibt.lt/home/xray/svn-repositories/cif-tools/trunk/CIFParser/shtests/text_field_001.sh $
+#------------------------------------------------------------------------------
+#*
+#  Perl test driver.
+#**
+
+use strict;
+
+use lib ".";
+use lib "lib/perl5";
+use lib "CIFParser";
+
+use File::Basename;
+use CIFParser;
+use ShowStruct;
+
+my $script_dir  = File::Basename::dirname( $0 );
+my $script_name = File::Basename::basename( $0 );
+
+$script_name =~ s/\.sh$//;
+
+my $filename = "${script_dir}/${script_name}.inp";
+
+my $parser = new CIFParser;
+
+my %options;
+
+$options{allow_uqstring_brackets} = 0;
+
+my $data = $parser->Run($filename, \%options);
+
+showRef($data);
+
+while(my($k,$v) = each %{$data->[0]{values}}) {
+    print "Values for '$k':\n";
+    for my $mas(@{$v}) {
+        print $mas . "\n";
+    }
+    print "-" x 20 . "\n";
+}
+
+exit 0;
