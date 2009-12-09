380d775c3c2b2087adbf8b96fb22d4ff7f0e34a8
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2009-12-09 09:40:15 +0000

    trunk/ Rewriting the cif_find_duplicates_006.sh shell-driven test so that it's STDOUT and STDERR are not mixed.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@1008 04be6746-3802-0410-999d-98508da1e98c

--- perl-scripts/shtests/cif_find_duplicates_006.sh
+++ perl-scripts/shtests/cif_find_duplicates_006.sh
@@ -7,4 +7,18 @@ unset LC_CTYPE
 
 find_numbers=./cif_find_duplicates
 
-${find_numbers} ./inputs/cifs-with-errors ./inputs/cod-with-errors | sort
+BASENAME="`basename $0 .sh`"
+TMP_DIR="./tmp-${BASENAME}"
+mkdir ${TMP_DIR}
+
+TMP_OUT="${TMP_DIR}/${find_numbers}.out"
+TMP_ERR="${TMP_DIR}/${find_numbers}.err"
+
+${find_numbers} ./inputs/cifs-with-errors ./inputs/cod-with-errors \
+    > ${TMP_OUT} \
+    2> ${TMP_ERR}
+
+cat ${TMP_OUT}
+cat ${TMP_ERR}
+
+rm -rf ${TMP_DIR}
