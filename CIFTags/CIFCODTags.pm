6c297116f77077307136222c2330d5ac8ae81d6b
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2009-12-11 12:01:01 +0000

    trunk/ Adding '_[local]_cod_text' and '_cod_text' tags into the CIFCODTags.pm tag list.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@1018 04be6746-3802-0410-999d-98508da1e98c

--- CIFTags/CIFCODTags.pm
+++ CIFTags/CIFCODTags.pm
@@ -39,6 +39,7 @@ _[local]_cod_related_suboptimal_struct
 _[local]_cod_chemical_formula_sum_orig
 _[local]_cod_chemical_formula_moiety_orig
 _[local]_cod_citation_special_details
+_[local]_cod_text
 
 _cod_duplicate_entry
 _cod_superseeded_by
@@ -62,6 +63,7 @@ _cod_related_suboptimal_struct
 _cod_chemical_formula_sum_orig
 _cod_chemical_formula_moiety_orig
 _cod_citation_special_details
+_cod_text
 );
 
 1;
