97a67449906c52e22fbfea5e1a8c3efbe9d08fe0
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2010-12-16 06:04:41 +0000

    trunk/ Fixing sprint_value() in CIFTagPrint.pm: it now prints values that start with a dollar sign enquoted in single quotes.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@1515 04be6746-3802-0410-999d-98508da1e98c

--- CIFTags/CIFTagPrint.pm
+++ CIFTags/CIFTagPrint.pm
@@ -306,7 +306,7 @@ sub sprint_value
         $val = "'" . $val . "'";
     } elsif( $val =~ /^'.*'$/ ) {
         $val = "\"" . $val . "\"";
-    } elsif( $val =~ /\s|^_|^\[|^".*"$/) {
+    } elsif( $val =~ /\s|^_|^\[|^\$|^".*"$/) {
         $val = "'" . $val . "'";
     }
     return $val;
