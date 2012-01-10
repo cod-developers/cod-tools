8b5f9d78a4281882761818f17d3d69e579185f8a
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2012-01-10 08:05:29 +0000

    trunk/CIFParser-c/ Adding explicit declarations of public functions to header files.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@1888 04be6746-3802-0410-999d-98508da1e98c

--- CIFParser-c/cif_grammar_flex.h
+++ CIFParser-c/cif_grammar_flex.h
@@ -26,6 +26,16 @@ int cif_flex_current_position( void );
 void cif_flex_set_current_position( ssize_t pos );
 const char *cif_flex_current_line( void );
 
+int cif_flex_previous_line_number( void );
+int cif_flex_previous_position( void );
+const char *cif_flex_previous_line( void );
+
+void set_lexer_fix_ctrl_z( void );
+void set_lexer_fix_non_ascii_symbols( void );
+void set_lexer_fix_missing_closing_double_quote( void );
+void set_lexer_fix_missing_closing_single_quote( void );
+void set_lexer_allow_uqstring_brackets( void );
+
 void cif_flex_push_state( FILE *replace_yyin, cexception_t *ex );
 void cif_flex_pop_state( void );
 
