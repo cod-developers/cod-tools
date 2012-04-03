9d32caa138bcc7cc0e2156b92b8c536898376eb2
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2012-04-03 14:39:09 +0000

    trunk/ Adding translated atom search to the 'CIFClassifyer.pm'.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@2042 04be6746-3802-0410-999d-98508da1e98c

--- CIFData/CIFClassifyer.pm
+++ CIFData/CIFClassifyer.pm
@@ -28,6 +28,12 @@ require Exporter;
 
 sub get_atoms( $$$ );
 
+sub mvmul( $$ );
+sub vdot( $$ );
+sub vsub( $$ );
+sub length_of_fractional_vector( $$ );
+sub distance_fractional( $$$ );
+
 sub cif_class_flags( $$$$ )
 {
     my ( $datablock, $filename, $atom_properties, $bond_safety_margin ) = @_;
@@ -60,11 +66,13 @@ sub cif_has_C_H_bond( $$$$ )
     ## use Serialise;
     ## serialiseRef( $atoms );
 
-
     my $symops =
         CIFSymmetryGenerator::get_symmetry_operators( $datablock, $filename );
 
     my @symop_matrices = map { symop_from_string($_) } @{$symops};
+
+    ## use Serialise;
+    ## serialiseRef( $symops );
 
     my @cell =
         CIFSymmetryGenerator::get_cell( $datablock->{values}, $filename,
@@ -76,8 +84,10 @@ sub cif_has_C_H_bond( $$$$ )
 
     my $f2o = symop_ortho_from_fract(  @cell );
 
+    my $g = metric_tensor_from_cell( @cell );
+
     ## use Serialise;
-    ## serialiseRef( $symops );
+    ## serialiseRef( $g );
 
     my $sym_atoms =
         CIFSymmetryGenerator::symop_generate_atoms( \@symop_matrices, 
@@ -91,6 +101,11 @@ sub cif_has_C_H_bond( $$$$ )
     for my $i (0..$#$atoms) {
         my $atom1 = $atoms->[$i];
         next unless $atom1->{atom_type} eq "C";
+
+        # First, let's try bonds with untranslated symmetry equivalent
+        # atoms, since they are the most probable -- asymmetric units
+        # usually contain atoms that are close to each other in a
+        # molecule:
         for my $j ($i+1..$#$sym_atoms) {
             my $atom2 = $sym_atoms->[$j];
             next unless $atom2->{atom_type} eq "H";
@@ -102,12 +117,44 @@ sub cif_has_C_H_bond( $$$$ )
                 CIFSymmetryGenerator::distance( $atom1->{coordinates_ortho},
                                                 $atom2->{coordinates_ortho} );
 
+
+            ## my $distance = distance_fractional( $atom1->{coordinates_fract},
+            ##                                     $atom2->{coordinates_fract},
+            ##                                     $g );
+            ## 
+            ## print ">>> $interatomic_distance, $distance\n"
+            ##     if abs($interatomic_distance - $distance) > 1E-6;
+
             if( $interatomic_distance < $C_H_covalent_distance ) {
                 ## use Serialise;
                 ## serialiseRef( [ $atom1, $atom2, $interatomic_distance,
                 ##                 $C_H_covalent_distance  ] );
                 return 1;
             }
+        }
+        # If we did not find bond to untranslated atoms, let's try
+        # +/-1 translation:
+        for my $j ($i+1..$#$sym_atoms) {
+            my $atom2 = $sym_atoms->[$j];
+            next unless $atom2->{atom_type} eq "H";
+            next if
+                $atom1->{assembly} eq $atom2->{assembly} &&
+                $atom1->{group} ne $atom2->{group};
+
+            for my $dx (-1, 1) {
+            for my $dy (-1, 1) {
+            for my $dz (-1, 1) {
+                my @shifted_fractionals = ( $atom2->{coordinates_fract}[0] + $dx,
+                                            $atom2->{coordinates_fract}[1] + $dy,
+                                            $atom2->{coordinates_fract}[2] + $dz );
+
+                my $distance = distance_fractional( $atom1->{coordinates_fract},
+                                                    \@shifted_fractionals,
+                                                    $g );
+                if( $distance < $C_H_covalent_distance ) {
+                    return 1;
+                }
+            }}}
         }
     }
 
@@ -208,6 +255,78 @@ sub get_atoms($$$)
     }
 
     return \@atoms;
+}
+
+my $Pi = 4 * atan2(1,1);
+
+sub metric_tensor_from_cell
+{
+    my @cell = @_;
+    my ($a, $b, $c) = @cell[0..2];
+    my ($alpha, $beta, $gamma) = map {$Pi * $_ / 180} @cell[3..5];
+    my ($ca, $cb, $cg) = map {cos} ($alpha, $beta, $gamma);
+
+    my $g = [
+        [ $a * $a, $a * $b * $cg, $a * $c * $cb ],
+        [ 0,       $b * $b,       $b * $c * $ca ],
+        [ 0,             0,       $c * $c ]
+        ];
+
+    $g->[1][0] = $g->[0][1];
+    $g->[2][0] = $g->[0][2];
+    $g->[2][1] = $g->[1][2];
+
+    return $g;
+}
+
+sub mvmul( $$ )
+{
+    my ($m, $v) = @_;
+    my @r;
+
+    for( my $i = 0; $i <= $#$m; $i++ ) {
+        $r[$i] = 0;
+        for( my $j = 0; $j <= $#{$m->[$i]}; $j++ ) {
+            $r[$i] += $m->[$i][$j] * $v->[$j]
+        }
+    }
+    return wantarray? @r : \@r
+}
+
+sub vdot( $$ )
+{
+    my ($v1, $v2) = @_;
+    my $r = 0;
+
+    for( my $i = 0; $i <= $#$v1; $i++ ) {
+        $r += $v1->[$i] * $v2->[$i]
+    }
+
+    return $r;
+}
+
+sub vsub( $$ )
+{
+    my ($v1, $v2) = @_;
+    my @r = ();
+
+    for( my $i = 0; $i <= $#$v1; $i++ ) {
+        $r[$i] = $v1->[$i] - $v2->[$i]
+    }
+
+    return wantarray? @r : \@r;    
+}
+
+sub length_of_fractional_vector( $$ )
+{
+    my ( $v, $g ) = @_;
+    return vdot( $v, mvmul( $g, $v ));
+}
+
+sub distance_fractional( $$$ )
+{
+    my ( $v1, $v2, $g ) = @_;
+    return sqrt( length_of_fractional_vector( vsub($v1, $v2), $g ));
 }
 
 1;
