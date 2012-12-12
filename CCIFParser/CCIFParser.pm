bfec00073fdf90e2e522b0edcd0f15d976df7409
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2012-12-12 11:22:12 +0000

    trunk/CCIFParser/ Introducing a workaround for a bug (?) of Perl Inline module -- passing paths to included directories (configuration option INC) for Perl Inline module via array, as passing as joined string causes errors. Too bad, same technique does not work with the paths to included libraries (configuration option LIBS), so it is left as is.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@2128 04be6746-3802-0410-999d-98508da1e98c

--- CCIFParser/CCIFParser.pm
+++ CCIFParser/CCIFParser.pm
@@ -26,9 +26,9 @@ use Inline C => Config =>
             "-L" . dirname(abs_path($0)) . "/../CIFParser-c/libs/cexceptions " .
             "-L" . dirname(abs_path($0)) . "/../CIFParser-c/libs/getoptions " .
             "-lCIFParser-c -lcexceptions -lgetoptions",
-    INC => "-I" . dirname(abs_path($0)) . "/../CIFParser-c " .
-           "-I" . dirname(abs_path($0)) . "/../CIFParser-c/libs/cexceptions " .
-           "-I" . dirname(abs_path($0)) . "/../CIFParser-c/libs/getoptions",
+    INC => [ "-I" . dirname(abs_path($0)) . "/../CIFParser-c",
+             "-I" . dirname(abs_path($0)) . "/../CIFParser-c/libs/cexceptions",
+             "-I" . dirname(abs_path($0)) . "/../CIFParser-c/libs/getoptions" ],
     DIRECTORY =>  dirname(abs_path($0)) . "/_Inline";
 use Inline C => <<'END_OF_C_CODE';
 
