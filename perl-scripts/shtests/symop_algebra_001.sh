e173eb3b7f9108fab0a8f66aa5028cbae0187212
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2013-07-11 12:04:02 +0000

    cod-tools/ (saulius@koala.ibt.lt) Starting module of symmetry operator algerbra that accepts three different symop representations as matrices.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@2182 04be6746-3802-0410-999d-98508da1e98c

---
+++ perl-scripts/shtests/symop_algebra_001.sh
@@ -0,0 +1,78 @@
+#! /bin/sh
+#!perl -w # --*- Perl -*--
+eval 'exec perl -x $0 ${1+"$@"}'
+    if 0;
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#  Unit-test the Spacegroups::SymopAlgebra module
+#**
+
+use strict;
+use warnings;
+
+use Spacegroups::SymopAlgebra;
+use Spacegroups::SymopParse;
+
+sub print_symop_product
+{
+    my ($symop1, $symop2 ) = @_;
+
+    SymopParse::symop_print( $symop1 );
+    print "\n";
+    SymopParse::symop_print( $symop2 );
+    print "\n";
+
+    my $symop3 = SymopAlgebra::symop_mul( $symop1, $symop2 );
+
+    SymopParse::symop_print( $symop3 );
+    print "=" x 79, "\n";
+}
+
+print_symop_product(
+    [[ 1, 0, 0 ],
+     [ 0,-1, 0 ],
+     [ 0, 0,-1 ]],
+
+    [[-1, 0, 0 ],
+     [ 0, 1, 0 ],
+     [ 0, 0,-1 ]]
+);
+
+print_symop_product(
+    [[ 1, 0, 0, 0.5],
+     [ 0,-1, 0, 0  ],
+     [ 0, 0,-1, 0  ],
+     [ 0, 0, 0, 1  ]],
+
+    [[-1, 0, 0, 0  ],
+     [ 0, 1, 0, 0.5],
+     [ 0, 0,-1, 0  ],
+     [ 0, 0, 0, 1  ]]
+);
+
+print_symop_product(
+    [[ 1, 0, 0, 0.5],
+     [ 0,-1, 0, 0  ],
+     [ 0, 0,-1, 0  ]],
+
+    [[-1, 0, 0, 0  ],
+     [ 0, 1, 0, 0.5],
+     [ 0, 0,-1, 0  ]]
+);
+
+print_symop_product(
+    [[ 1, 0, 0 ],
+     [ 0,-1, 0 ],
+     [ 0, 0,-1 ],
+     [ 0.5, 0, 0 ]],
+
+    [[-1, 0, 0 ],
+     [ 0, 1, 0 ],
+     [ 0, 0,-1 ],
+     [ 0, 0.5, 0 ]]
+);
