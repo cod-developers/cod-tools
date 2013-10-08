40a4d1b8a94d1b65b190bac2f6b6f65c3b24c63d
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2013-10-08 07:49:13 +0000

    trunk/ Adding package 'sqlite3' to the dependency list.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@2300 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Ubuntu-12.04/install.sh
+++ dependencies/Ubuntu-12.04/install.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get install -y libparse-yapp-perl
 
+sudo apt-get install -y sqlite3
 sudo apt-get install -y libdbi-perl
 sudo apt-get install -y libdbd-sqlite3-perl
 sudo apt-get install -y libcapture-tiny-perl
