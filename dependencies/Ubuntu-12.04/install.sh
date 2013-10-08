03525c583c08f3c30e611c593a7fb0fc9c5fd9de
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2013-10-08 07:47:54 +0000

    trunk/ Adding packages 'libdbi-perl' and 'libdbd-sqlite3-perl' to dependency list.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@2299 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Ubuntu-12.04/install.sh
+++ dependencies/Ubuntu-12.04/install.sh
@@ -2,5 +2,7 @@
 
 sudo apt-get install -y libparse-yapp-perl
 
+sudo apt-get install -y libdbi-perl
+sudo apt-get install -y libdbd-sqlite3-perl
 sudo apt-get install -y libcapture-tiny-perl
 sudo apt-get install -y libcarp-assert-perl
