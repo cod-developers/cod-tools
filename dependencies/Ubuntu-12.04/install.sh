edfc7168b4b740b4f24ab316f3f26ea497c5efdb
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2013-12-12 07:54:39 +0000

    trunk/dependencies/ (saulius@koala.ibt.lt) Adding an 'obabel' dependency to the 'Ubuntu-12.04/install.sh' script.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@2391 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Ubuntu-12.04/install.sh
+++ dependencies/Ubuntu-12.04/install.sh
@@ -8,3 +8,4 @@ sudo apt-get install -y libdbd-sqlite3-perl
 sudo apt-get install -y libcapture-tiny-perl
 sudo apt-get install -y libcarp-assert-perl
 sudo apt-get install -y libdigest-sha-perl
+sudo apt-get install -y openbabel
