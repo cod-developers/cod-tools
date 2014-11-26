84c7a742a537780167039704db319193c3f42922
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2014-11-26 16:02:39 +0000

    trunk/ Adding code to KG76.pm for the construction of change-of-basis matrices according to the paper by Grosse-Kunstleve, Sauter & Adams.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@2963 04be6746-3802-0410-999d-98508da1e98c

--- lib/perl5/COD/Cell/Niggli/KG76.pm
+++ lib/perl5/COD/Cell/Niggli/KG76.pm
@@ -30,6 +30,7 @@ package KG76;
 
 use strict;
 use warnings;
+use Spacegroups::SymopAlgebra;
 require Exporter;
 @KG76::ISA = qw(Exporter);
 @KG76::EXPORT_OK = qw( reduce );
@@ -68,8 +69,11 @@ sub reduce
         # 1.
         if( $A - $eps > $B or
             (abs($A-$B) < $eps && abs($ksi) - $eps > abs($eta)) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "1.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "1.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
+            my $Cm = [ [0,-1,0], [-1,0,0], [0,0,-1] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             my ($tmp1, $tmp2) = ($A, $ksi);
             ($A, $ksi) = ($B, $eta);
             ($B, $eta) = ($tmp1, $tmp2);
@@ -77,8 +81,11 @@ sub reduce
         # 2.
         if( $B - $eps > $C or
             (abs($B-$C) < $eps && abs($eta) - $eps > abs($dzeta)) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "2.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "2.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
+            my $Cm = [ [-1,0,0], [0,0,-1], [0,-1,0] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             my ($tmp1, $tmp2) = ($B, $eta);
             ($B, $eta) = ($C, $dzeta);
             ($C, $dzeta) = ($tmp1, $tmp2);
@@ -86,14 +93,42 @@ sub reduce
         }
         # 3.
         if( $ksi * $eta * $dzeta - $eps > 0 ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "3.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "3.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
+            my $i = ($ksi < 0)   ? -1 : 1;
+            my $j = ($eta < 0)   ? -1 : 1;
+            my $k = ($dzeta < 0) ? -1 : 1;
+            my $Cm = [ [$i,0,0], [0,$j,0], [0,0,$k] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             ($ksi, $eta, $dzeta) = map {abs} ($ksi, $eta, $dzeta);
         }
         # 4.
         if( !($ksi * $eta * $dzeta - $eps > 0) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "4.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "4.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
+            my ($i, $j, $k, $p) = (1, 1, 1);
+            if( $ksi - $eps > 0 ) {
+                $i = -1;
+            } elsif( !($ksi < 0) ) {
+                $p = \$i;
+            }
+            if( $eta - $eps > 0 ) {
+                $j = -1;
+            } elsif( !($eta < 0) ) {
+                $p = \$j
+            }
+            if( $dzeta - $eps > 0 ) {
+                $k = -1;
+            } elsif( !($dzeta < 0) ) {
+                $p = \$k;
+            }
+            if( $i * $j * $k < 0 ) {
+                $$p = -1;
+            }
+            my $Cm = [ [$i,0,0], [0,$j,0], [0,0,$k] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             ($ksi, $eta, $dzeta) = map {-abs($_)} ($ksi, $eta, $dzeta);
         }
         # 5.
@@ -101,9 +136,12 @@ sub reduce
         if( abs($ksi) - $eps > $B or 
             (abs($ksi-$B) < $eps && 2 * $eta < $dzeta - $eps) or
             (abs($ksi+$B) < $eps && $dzeta < -$eps) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "5.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "5.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
             my $sign_ksi = $ksi > 0 ? 1 : -1;
+            my $Cm = [ [1,0,0], [0,1,-$sign_ksi], [0,0,1] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             $C = $B + $C - $ksi * $sign_ksi;
             $eta -= $dzeta * $sign_ksi;
             $ksi -= 2 * $B * $sign_ksi;
@@ -113,9 +151,12 @@ sub reduce
         if( abs($eta) - $eps > $A or
             (abs($eta-$A) < $eps && 2 * $ksi < $dzeta - $eps) or
             (abs($eta+$A) < $eps && $dzeta < -$eps) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "6.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "6.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
             my $sign_eta = $eta > 0 ? 1 : -1;
+            my $Cm = [ [1,0,-$sign_eta], [0,1,0], [0,0,1] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             $C = $A + $C - $eta * $sign_eta;
             $ksi -= $dzeta * $sign_eta;
             $eta -= 2 * $A * $sign_eta;
@@ -126,9 +167,12 @@ sub reduce
         if( abs($dzeta) - $eps > $A or
             (abs($dzeta-$A) < $eps && 2 * $ksi < $eta - $eps) or
             (abs($dzeta+$A) < $eps && $eta < -$eps) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "7.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "7.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
             my $sign_dzeta = $dzeta > 0 ? 1 : -1;
+            my $Cm = [ [1,-$sign_dzeta,0], [0,1,0], [0,0,1] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             $B = $A + $B - $dzeta * $sign_dzeta;
             $ksi -= $eta * $sign_dzeta;
             $dzeta -= 2 * $A * $sign_dzeta;
@@ -138,8 +182,11 @@ sub reduce
         if( $ksi + $eta + $dzeta + $A + $B < -$eps or
             (abs($ksi + $eta + $dzeta + $A + $B) < $eps && 
              2*($A+$eta) + $dzeta - $eps > 0) ) {
-            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",
-            "8.", $A, $B, $C, $ksi, $eta, $dzeta, " " if $KG76::debug;
+            printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n",
+            "8.", $A, $B, $C, $ksi, $eta, $dzeta,
+            join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
+            my $Cm = [ [1,0,1], [0,1,1], [0,0,1] ];
+            $CoB = SymopAlgebra::symop_mul( $CoB, $Cm );
             $C = $A + $B + $C + $ksi + $eta + $dzeta;
             $ksi = 2*$B + $ksi + $dzeta;
             $eta = 2*$A + $eta + $dzeta;
@@ -149,17 +196,22 @@ sub reduce
     }
     if( $KG76::debug ) {
         use POSIX;
-        printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n", 
-        "  ", $A, $B, $C, $ksi, $eta, $dzeta, " ";
+        printf "%s  %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f [[%s]]\n", 
+        "  ", $A, $B, $C, $ksi, $eta, $dzeta,
+        join("], [", map {join(",", @$_)} @$CoB) if $KG76::debug;
         print "CELL", sqrt($A), sqrt($B), sqrt($C),
         180*acos($ksi/(2*sqrt($B)*sqrt($C)))/$Pi,
         180*acos($eta/(2*sqrt($A)*sqrt($C)))/$Pi,
         180*acos($dzeta/(2*sqrt($A)*sqrt($B)))/$Pi;
+        print "COB", @{$CoB->[0]};
+        print "COB", @{$CoB->[1]};
+        print "COB", @{$CoB->[2]};
     }
     return (sqrt($A), sqrt($B), sqrt($C),
             180*acos($ksi/(2*sqrt($B)*sqrt($C)))/$Pi,
             180*acos($eta/(2*sqrt($A)*sqrt($C)))/$Pi,
-            180*acos($dzeta/(2*sqrt($A)*sqrt($B)))/$Pi);
+            180*acos($dzeta/(2*sqrt($A)*sqrt($B)))/$Pi,
+            $CoB);
 }
 
 1;
