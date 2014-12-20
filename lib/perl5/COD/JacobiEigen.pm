80e9b93f3129f3019ca3f8ad2ab11449f67ea17a
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2014-12-20 21:24:33 +0000

    trunk/lib/perl5/ (antanas@echidna.ibt.lt) Replacing tabs with spaces, adding svn keywords to JacobiEigen.pm.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@2984 04be6746-3802-0410-999d-98508da1e98c

--- lib/perl5/COD/JacobiEigen.pm
+++ lib/perl5/COD/JacobiEigen.pm
@@ -1,11 +1,10 @@
 #--*-perl-*-------------------------------------------------------------
-#$Author: saulius $
-#$Date: 2006-05-12 09:40:28 +0300 (Fri, 12 May 2006) $
-#$Revision: 1056 $
-#$URL: svn+ssh://lokys.ibt.lt/home/saulius/svn-repositories/cce/modules/JacobiEigen.pm $
+#$Author$
+#$Date$
+#$Revision$
+#$URL$
 #-----------------------------------------------------------------------
-# find all eigenvalues and eigenvectors of a symmetric matrix a of
-# order n.
+# Find all eigenvalues and eigenvectors of a symmetric matrix a of order n.
 #-----------------------------------------------------------------------
 
 package JacobiEigen;
@@ -34,18 +33,18 @@ sub jacobi_eigenvv ## ( @ )
     my $n = scalar(@a);
     for( $i = 0; $i < $n; $i++ ) {
         $eigenvalues[$i] = $a[$i][$i];
-	for( $j = 0; $j < $n; $j++ ) {
-	    $eigenvectors[$i][$j] = 0.0;
-	}
-	$eigenvectors[$i][$i] = 1.0;
+        for( $j = 0; $j < $n; $j++ ) {
+            $eigenvectors[$i][$j] = 0.0;
+        }
+        $eigenvectors[$i][$i] = 1.0;
     }
 
     for( $sweep_count = 0; $sweep_count < $JacobiEigen::max_iterations; 
-	 $sweep_count++ ) {
+         $sweep_count++ ) {
         if( &sum_off_diag(@a) < $JacobiEigen::delta ) {
-	    return ( \@eigenvectors, \@eigenvalues, $sweep_count );
-	}
-	&jacobi_sweep( \@a, \@eigenvalues, \@eigenvectors );
+            return ( \@eigenvectors, \@eigenvalues, $sweep_count );
+        }
+        &jacobi_sweep( \@a, \@eigenvalues, \@eigenvectors );
     }
     die( "Too many iterations in jacobi_eigenvv()" );
 }
@@ -59,8 +58,8 @@ sub sum_off_diag( @ )
     my $n = scalar(@a);
     for( $i = 0; $i < $n - 1; $i++ ) {
         for( $j = $i + 1; $j < $n; $j++ ) {
-	    $sum += abs($a[$i][$j]);
-	}
+            $sum += abs($a[$i][$j]);
+        }
     }
 
     return $sum;
@@ -69,7 +68,7 @@ sub sum_off_diag( @ )
 sub jacobi_transform($$$$$$$)
 {
     my( $m, $i, $j, $k, $l, $c, $s ) = @_;
-    
+
     my $g = $m->[$i][$j]; my $h = $m->[$k][$l];
     $m->[$i][$j] =  $g * $c + $h * $s;
     $m->[$k][$l] =  $h * $c - $g * $s;
@@ -84,32 +83,32 @@ sub jacobi_sweep( $$$ )
     my $n = scalar(@{$a});
     for( $p = 0; $p < $n-1; $p++ ) {
         for( $q = $p+1; $q < $n; $q++ ) {
-	    $h = $d->[$p] - $d->[$q];
-	    $R = sqrt($h*$h + 4*$a->[$p][$q]*$a->[$p][$q]);
-	    if( $d->[$p] > $d->[$q] ) {
-	        $c = sqrt(0.5 + $h/(2*$R));
-		$s = $a->[$p][$q]/($R*$c);
-	    } else {
-	        $s = sqrt(0.5 - $h/(2*$R));
-		$c = $a->[$p][$q]/($R*$s);
-	    }
-	    $h = $d->[$p] + $d->[$q];
-	    $d->[$p] = 0.5*($h+$R);
-	    $d->[$q] = 0.5*($h-$R);
-	    $a->[$p][$q] = 0.0;
-	    for( $r = 0; $r < $p; $r++ ) {
-	        jacobi_transform( $a,  $r, $p,  $r, $q, $c, $s );
-	    }
-	    for( $r = $p + 1; $r < $q; $r++ ) {
-	        jacobi_transform( $a,  $p, $r,  $r, $q, $c, $s );
-	    }
-	    for( $r = $q + 1; $r < $n; $r++ ) {
-	        jacobi_transform( $a,  $p, $r,  $q, $r, $c, $s );
-	    }
-	    for( $r = 0; $r < $n; $r++ ) {
-	        jacobi_transform( $v,  $r, $p,  $r, $q, $c, $s );
-	    }
-	}
+            $h = $d->[$p] - $d->[$q];
+            $R = sqrt($h*$h + 4*$a->[$p][$q]*$a->[$p][$q]);
+            if( $d->[$p] > $d->[$q] ) {
+                $c = sqrt(0.5 + $h/(2*$R));
+                $s = $a->[$p][$q]/($R*$c);
+            } else {
+                $s = sqrt(0.5 - $h/(2*$R));
+                $c = $a->[$p][$q]/($R*$s);
+            }
+            $h = $d->[$p] + $d->[$q];
+            $d->[$p] = 0.5*($h+$R);
+            $d->[$q] = 0.5*($h-$R);
+            $a->[$p][$q] = 0.0;
+            for( $r = 0; $r < $p; $r++ ) {
+                jacobi_transform( $a,  $r, $p,  $r, $q, $c, $s );
+            }
+            for( $r = $p + 1; $r < $q; $r++ ) {
+                jacobi_transform( $a,  $p, $r,  $r, $q, $c, $s );
+            }
+            for( $r = $q + 1; $r < $n; $r++ ) {
+                jacobi_transform( $a,  $p, $r,  $q, $r, $c, $s );
+            }
+            for( $r = 0; $r < $n; $r++ ) {
+                jacobi_transform( $v,  $r, $p,  $r, $q, $c, $s );
+            }
+        }
     }
 }
 
