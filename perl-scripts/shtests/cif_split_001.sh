9c354ab14a14368252a4d1f8b41b808c7c166456
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2015-02-23 14:58:33 +0000

    trunk/ Adding cif_split, a sophisticated CIF splitting script, which parses given CIF files to separate the datablocks. Thus, the script is capable of splitting non-correctly formatted and nested CIF files.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@3077 04be6746-3802-0410-999d-98508da1e98c

---
+++ perl-scripts/shtests/cif_split_001.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -ue
+
+cif_split=./cif_split
+
+CIF=./inputs/Cottone_2002_p418_crude.cif
+
+BASENAME="`basename $0 .sh`"
+
+TMP_DIR="./tmp-${BASENAME}"
+
+mkdir ${TMP_DIR}
+
+${cif_split} < ${CIF} -o ${TMP_DIR}
+
+for i in $(find ${TMP_DIR} -name \*.cif | sort)
+do
+    cat $i | xargs -i echo "$i: {}"
+done
+
+rm -rf ${TMP_DIR}
