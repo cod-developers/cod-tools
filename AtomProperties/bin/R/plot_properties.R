4602fb7c5b9ab3ef59880fb116115b325614fb53
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2015-03-30 11:35:28 +0000

    trunk/ (antanas@echidna.ibt.lt) Copying 'AtomProperties' folder from 'antanas-BO-atomProperties' branch which holds the core new functionality.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@3147 04be6746-3802-0410-999d-98508da1e98c

---
+++ AtomProperties/bin/R/plot_properties.R
@@ -0,0 +1,11 @@
+plot_element <- function(filename) {
+    ele = read.table(filename, header=T)
+    x <- seq(1, length(ele[[1]]))
+    max_y = max(ele[,2:ncol(ele)])
+    plot(x, xlab="Element number", type="n", 
+         ylab="Covalent radius (\uc5)", ylim=c(0, max_y))
+    for (i in 2:ncol(ele)) {
+        lines(x, ele[[i]], type="l", col=i )
+    }
+    legend(1, 2.5, names(ele)[2:ncol(ele)],  lty=c(1,1), col = seq(2,ncol(ele)))
+}
