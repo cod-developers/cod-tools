bd8e31b17ed21bebebf82d6081f868f6637fab74
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2015-04-20 21:34:38 +0000

    cod-tools/ (antanas@kurmis) Modifying 'cif_distances' module dependencies to avoid error while using the sh based way to select the Perl interpreter.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@3269 04be6746-3802-0410-999d-98508da1e98c

--- CIFData/CIFSymmetryGenerator.pm
+++ CIFData/CIFSymmetryGenerator.pm
@@ -21,12 +21,13 @@ use Spacegroups::SpacegroupNames;
 use UserMessage;
 
 require Exporter;
-@CIFSymmetryGenerator::ISA = qw(Exporter);
-@CIFSymmetryGenerator::EXPORT = qw(
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
     get_cell
     get_symmetry_operators
     symop_generate_atoms
 );
+our @EXPORT_OK = qw ( copy_atom );
 
 my %sg_name_abbrev =
     map { my $key = $_->[1]; $key =~ s/\s+//g; ( $key, $_->[2] ) }
