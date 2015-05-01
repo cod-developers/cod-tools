1778046714d023751c15d4f6f7648fe96eafebeb
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2015-05-01 14:49:22 +0000

    trunk/ Moving subroutine distance() from CIFSymmetryGenerator to more appropriate module VectorAlgebra.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@3344 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIFData/CIFClassifyer.pm
+++ src/lib/perl5/COD/CIFData/CIFClassifyer.pm
@@ -16,11 +16,11 @@ use warnings;
 
 use COD::Spacegroups::SymopParse;
 use COD::Fractional;
-use COD::CIFData::CIFSymmetryGenerator qw( distance
-                                           get_cell
+use COD::CIFData::CIFSymmetryGenerator qw( get_cell
                                            get_symmetry_operators
                                            mat_vect_mul
                                            symop_generate_atoms );
+use COD::Spacegroups::VectorAlgebra qw(distance);
 use COD::UserMessage;
 
 require Exporter;
