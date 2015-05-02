6a06b8902c8f1a07cb9fdc37e60d42d5a8bd3164
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2015-05-02 15:10:25 +0000

    trunk/src/lib/perl5/COD/ (saulius@koala.ibt.lt) Adding the first implamentation of the Delaunay cell reduction algorithm.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@3353 04be6746-3802-0410-999d-98508da1e98c

---
+++ src/lib/perl5/COD/Cell/Delaunay/Delaunay.pm
@@ -0,0 +1,160 @@
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#  Perl package implementing the Delaunay cell reduction as decribed
+#  in the ITC vol. A and the spglib-1.6.4 library by Atsushi Togo.
+#**
+
+# To test this module, run:
+
+# perl -MDelaunay -le '
+#    $v = COD::Cell::Delaunay::Delaunay::reduce( 4.693, 4.936, 7.524, 131, 89.57, 90.67 );
+#    print join(" ", map {sprintf("%5.3f",$_)} @{$v->[0]})'
+
+package COD::Cell::Delaunay::Delaunay;
+
+use strict;
+use warnings;
+require Exporter;
+@COD::Cell::Delaunay::Delaunay::ISA = qw(Exporter);
+@COD::Cell::Delaunay::Delaunay::EXPORT_OK = qw( reduce );
+
+my $Pi = 4 * atan2(1,1);
+
+use COD::Fractional qw( symop_ortho_from_fract );
+use COD::Spacegroups::SymopAlgebra qw( symop_mul symop_apply );
+
+our $debug = 1;
+our $epsilon = 1E-5;
+
+sub reduce
+{
+    my @cell = @_;
+
+    my $f2o = symop_ortho_from_fract( @cell );
+
+    my $basis_vectors = [
+        symop_apply( $f2o, [1,0,0] ),
+        symop_apply( $f2o, [0,1,0] ),
+        symop_apply( $f2o, [0,0,1] )
+    ];
+
+    my $reduced_vectors = Delaunay_reduction( $basis_vectors );
+
+    my @reduced_cell = (
+        vlen(  $reduced_vectors->[0]),
+        vlen(  $reduced_vectors->[1]),
+        vlen(  $reduced_vectors->[2]),
+        vangle($reduced_vectors->[1], $reduced_vectors->[2]),
+        vangle($reduced_vectors->[0], $reduced_vectors->[2]),
+        vangle($reduced_vectors->[0], $reduced_vectors->[1])
+    );
+
+    return [ \@reduced_cell, $reduced_vectors ];
+}
+
+sub vlen
+{
+    return sqrt( vlen2( $_[0] ));
+}
+
+sub vangle
+{
+    use Math::Trig;
+    my ($v1, $v2) = @_;
+    my $cosine = vdot( $v1, $v2 ) / ( vlen($v1) * vlen($v2) );
+    return 180*Math::Trig::acos($cosine)/$Pi;
+}
+
+# Delaunau reduction is described in the International Tables for
+# Crystallography, Vol. A.
+
+# The step code coded after the spglib-1.6.4 F/LOSS library by Atsushi
+# Togo.
+
+sub Delaunay_reduction
+{
+    my ($basis, $epsilon) = @_;
+    $epsilon = $COD::Cell::Delaunay::Delaunay::epsilon unless defined $epsilon;
+    my @extended_basis =
+        ( @$basis,
+          [ -$basis->[0][0]-$basis->[1][0]-$basis->[2][0],
+            -$basis->[0][1]-$basis->[1][1]-$basis->[2][1],
+            -$basis->[0][2]-$basis->[1][2]-$basis->[2][2],
+          ] );
+
+    my $step = 0;
+    while( Delaunay_reduction_step( \@extended_basis, $epsilon ) &&
+           $step < 10000 ) {
+        $step ++;
+    }
+
+    my $reduced_basis = Delaunay_minimal_vectors( \@extended_basis, $epsilon );
+    return $reduced_basis;
+}
+
+sub Delaunay_reduction_step
+{
+    my ($ebasis, $epsilon) = @_;
+
+    for( my $i = 0; $i < 4; $i ++ ) {
+        for( my $j = $i + 1; $j < 4; $j ++ ) {
+            my $dotprod = vdot( $ebasis->[$i], $ebasis->[$j] );
+            if( $dotprod > $epsilon ) {
+                for( my $k = 0; $k < 4; $k ++ ) {
+                    if( $k != $i && $k != $j ) {
+                        for( my $l = 0; $l < 3; $l ++ ) {
+                            $ebasis->[$k][$l] += $ebasis->[$i][$l];
+                        }
+                    }
+                }
+                for( my $l = 0; $l < 3; $l ++ ) {
+                    $ebasis->[$i][$l] = -$ebasis->[$i][$l];
+                }
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+sub vdot
+{
+    my ($v1, $v2) = @_;
+    return $v1->[0]*$v2->[0] + $v1->[1]*$v2->[1] + $v1->[2]*$v2->[2];
+}
+
+sub vsum
+{
+    my ($v1, $v2) = @_;
+    return [ $v1->[0] + $v2->[0], $v1->[1] + $v2->[1],  $v1->[2] + $v2->[2] ];
+}
+
+sub vlen2
+{
+    return vdot( $_[0], $_[0] );
+}
+
+sub Delaunay_minimal_vectors
+{
+    my ($ebasis, $epsilon) = @_;
+
+    my @candidates = (
+        @$ebasis,
+        vsum( $ebasis->[0], $ebasis->[1] ),
+        vsum( $ebasis->[1], $ebasis->[2] ),
+        vsum( $ebasis->[2], $ebasis->[0] )
+    );
+
+    my @lengths = 
+        sort { $a->[0] <=> $b->[0] }
+        map { [ vlen($_), $_ ] } @candidates;
+
+    return [ $lengths[0][1], $lengths[1][1], $lengths[2][1] ];
+}
+
+1;
