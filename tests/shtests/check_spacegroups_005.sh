fab8dc5192950445de6424b2754639e1fed16196
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2015-07-13 12:51:35 +0000

    cod-tools/ (saulius@koala.ibt.lt) Adding two more tests for the identification of the newly added non-standard setting.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@3494 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/check_spacegroups_005.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Test definition of the newly introduced nonstandard 'B 1 21/m 1'
+# spacegroup setting:
+
+set -ue
+
+./scripts/symop_build_spacegroup <<EOF
+-x,y+1/2,-z
+-x,-y,-z
+x+1/2,y,z+1/2
+EOF
