9975f8d6877f2dcf89a12b0bb7db9763cfcd81d7
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2015-09-21 14:29:16 +0000

    branches/ (antanas@echidna.ibt.lt) Refactoring parser message processing code to COD::ErrorHandler::process_parser_messages subroutine.
    git-svn-id: svn://cod.ibt.lt/cod-tools/branches/antanas-perl-error-message-handling@3955 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/ErrorHandler.pm
+++ src/lib/perl5/COD/ErrorHandler.pm
@@ -12,11 +12,42 @@ package COD::ErrorHandler;
 
 use strict;
 use warnings;
-use COD::UserMessage qw( sprint_message );
+use COD::UserMessage qw( sprint_message parse_message );
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw( process_warnings process_errors );
+our @EXPORT_OK = qw( process_warnings process_errors process_parser_messages );
 
+sub process_parser_messages
+{
+    my ($messages, $die_on_error_level, $print_order) = (@_);
+    $print_order = [ 'NOTE', 'WARNING', 'ERROR' ] if !defined $print_order;
+
+    my $filename;
+    my %err_level_counter;
+    foreach ( @$messages ) {
+        warn $_;
+        my $parsed_message = parse_message($_);
+        if ( defined $parsed_message->{errlevel} ) {
+            $err_level_counter{$parsed_message->{errlevel}}++;
+        };
+        if ( defined $parsed_message->{filename} && !defined $filename ) {
+            $filename = $parsed_message->{filename};
+        }
+    };
+
+    foreach ( @$print_order ) {
+        if ( defined $err_level_counter{$_} && $err_level_counter{$_} > 0 ) {
+            my $message = sprint_message( 
+                            $0, $filename, undef,
+                            $die_on_error_level->{$_} ? 'ERROR' : 'NOTE',
+                            "$err_level_counter{$_} $_(s) encountered "
+                          . 'while parsing the file',
+                            $die_on_error_level->{$_} ? 
+                            "die on $_(s) requested" : undef);
+            $die_on_error_level->{$_} ? die $message : warn $message;
+        }
+    };
+};
 
 sub process_errors
 {
