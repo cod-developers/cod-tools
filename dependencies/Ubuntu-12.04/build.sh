d74c74a9854f585ab62844f2657b765380c7e158
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2015-12-17 18:05:52 +0000

    trunk/dependencies/Ubuntu-12.04/ (antanas@kurmis) Enhancement: adding 'Autoconf' to the list of build dependencies since it is required by the LinuxMint-17.2 (shares sources with Ubuntu 14.04).
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@4472 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Ubuntu-12.04/build.sh
+++ dependencies/Ubuntu-12.04/build.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y bison
 sudo apt-get install -y flex
 sudo apt-get install -y gcc
 sudo apt-get install -y make
+sudo apt-get install -y Autoconf
 
 sudo apt-get install -y perl
 sudo apt-get install -y libparse-yapp-perl
