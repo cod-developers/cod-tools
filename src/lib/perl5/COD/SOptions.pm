8aa0fa7b19be4a8b99534758bc3f0e0f5845d620
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2016-01-12 14:18:28 +0000

    trunk/src/lib/perl5/ (antanas@echidna.ibt.lt) Bug fix: fixing some malformed error messages in the 'COD::SOptions' module.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@4527 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/SOptions.pm
+++ src/lib/perl5/COD/SOptions.pm
@@ -118,8 +118,8 @@ sub get_ints
     my $option = $args[0];
     my $value = get_value();
     if ( $value !~ /^ \s* (?:$integer\s*)+ \s* $/x ) {
-        die "$0:: option '$option' requires one or several integer "
-          . "arguments.\n";
+        die "$0:: ERROR, option '$option' requires one or several "
+          . "integer arguments.\n";
     }
     return $value;
 }
@@ -129,8 +129,8 @@ sub get_floats
     my $option = $args[0];
     my $value = get_value();
     if ($value !~ /^ \s* (?:$float\s*)+ \s* $/x) {
-        die "$0, option '$option' requires one or several floating-point "
-          . "arguments.\n";
+        die "$0: ERROR, option '$option' requires one or several "
+          . "floating-point arguments.\n";
     }
     return $value;
 }
