ab0db4d04ccb0430876ba4fe84e6ad2ef162d529
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2016-01-18 12:14:50 +0000

    trunk/ Issuing 'apt-get install' only once for each of 'build.sh' and 'run.sh' dependency installers thus saving time, which is otherwise wasted on building the dependency trees.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@4537 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Ubuntu-12.04/build.sh
+++ dependencies/Ubuntu-12.04/build.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-sudo apt-get install -y bison
-sudo apt-get install -y flex
-sudo apt-get install -y gcc
-sudo apt-get install -y make
-sudo apt-get install -y Autoconf
-
-sudo apt-get install -y perl
-sudo apt-get install -y libparse-yapp-perl
-sudo apt-get install -y libcarp-assert-perl
-sudo apt-get install -y swig
-sudo apt-get install -y python-dev
-
-sudo apt-get install -y libmodule-scandeps-perl
-
-sudo apt-get install -y moreutils
-
-sudo apt-get install -y debhelper
+sudo apt-get install -y \
+    autoconf \
+    bison \
+    debhelper \
+    flex \
+    gcc \
+    libcarp-assert-perl \
+    libmodule-scandeps-perl \
+    libparse-yapp-perl \
+    make \
+    moreutils \
+    perl \
+    python-dev \
+    swig
