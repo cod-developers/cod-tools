6c0a16767c357dbc2300a80ad64f02513faa14f5
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2016-03-14 08:30:56 +0000

    trunk/ Updating tag list from cif_dft.dic.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@4601 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/Tags/DFT.pm
+++ src/lib/perl5/COD/CIF/Tags/DFT.pm
@@ -5,14 +5,13 @@
 #$URL$
 #------------------------------------------------------------------------
 #* 
-#  A list of DFT CIF dictionary tags. Compiled from version 0.013 of
+#  A list of DFT CIF dictionary tags. Compiled from version 0.017 of
 #  cif_dft.dic:
 #
 #  URL: svn://www.crystallography.net/tcod/cif/dictionaries/cif_dft.dic
-#  Relative URL: ^/cif/dictionaries/cif_dft.dic
 #  Repository Root: svn://www.crystallography.net/tcod
 #  Repository UUID: f3fa9230-7ed6-4b69-bb24-8fd292b8517b
-#  Revision: 501
+#  Revision: 1560
 #**
 
 package COD::CIF::Tags::DFT;
@@ -50,6 +49,7 @@ _dft_atom_type_occupation
 _dft_atom_type_radius
 _dft_atom_type_valence_configuration
 _dft_atom_type_valence_electrons
+_dft_band_gap
 _dft_basisset_[]
 _dft_basisset_citation_id
 _dft_basisset_database_abbrev
