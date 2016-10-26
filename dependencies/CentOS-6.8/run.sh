335d4bcf8d6b8313f11a3ad0d9f7402c421a2a9f
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2016-10-26 05:07:25 +0000

    trunk/ (saulius@narvalas.ibt.lt) Adding more cpd-tools compile- and run-time dependencies for CentOS-6.8.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@4756 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/CentOS-6.8/run.sh
+++ dependencies/CentOS-6.8/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 sudo yum install -y \
+    perl-Digest-SHA \
     perl-Clone \
     perl-DBD-MySQL \
+    perl-DBD-SQLite \
     perl-JSON \
     perl-List-MoreUtils \
     perl-WWW-Curl
