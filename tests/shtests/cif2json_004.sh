d256a1fe9936610cc7238d894ebf03a56fed3eac
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-01-27 07:59:25 +0000

    branches/andrius-cif2json/ Adding script 'json2cif'.
    git-svn-id: svn://cod.ibt.lt/cod-tools/branches/andrius-cif2json@4821 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/cif2json_004.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#BEGIN DEPEND------------------------------------------------------------------
+
+INPUT_CIF2JSON=scripts/cif2json
+INPUT_CIF_CORRECT_TAGS=scripts/cif_correct_tags
+INPUT_JSON2CIF=scripts/json2cif
+INPUT_CIF=tests/inputs/4303187-multiple-underscores.cif
+
+#END DEPEND--------------------------------------------------------------------
+
+${INPUT_CIF2JSON} ${INPUT_CIF} \
+    | ${INPUT_CIF_CORRECT_TAGS} --json \
+    | ${INPUT_JSON2CIF}
