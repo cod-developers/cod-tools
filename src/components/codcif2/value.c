e04fc906f5a283aeae4ccc8263c1d814a2b7edaf
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-03-01 11:40:48 +0000

    branches/andrius-CIF2-parser/src/components/codcif2/ Adding functions to create new VALUEs from scalars and tables.
    git-svn-id: svn://cod.ibt.lt/cod-tools/branches/andrius-CIF2-parser@4987 04be6746-3802-0410-999d-98508da1e98c

--- src/components/codcif2/value.c
+++ src/components/codcif2/value.c
@@ -5,14 +5,30 @@
 * $URL$
 \*-------------------------------------------------------------------------*/
 
+#include <cexceptions.h>
+#include <allocx.h>
 #include <value.h>
+#include <table.h>
 
 struct VALUE {
     union {
         char *str;
-        struct VALUE *s;
         // struct LIST *l;
         struct TABLE *t;
     } v;
     value_type_t type;
 };
+
+VALUE *new_value_from_scalar( char *s, cexception_t *ex ) {
+    VALUE *value = callocx( 1, sizeof(VALUE), ex );
+    value->v.str = s;
+    value->type = VALUE_SCALAR;
+    return value;
+}
+
+VALUE *new_value_from_table( TABLE *t, cexception_t *ex ) {
+    VALUE *value = callocx( 1, sizeof(VALUE), ex );
+    value->v.t = t;
+    value->type = VALUE_TABLE;
+    return value;
+}
