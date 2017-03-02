f324341923ceb224e3f2fd7bb7b45cc422aa4307
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-03-02 08:09:39 +0000

    cod-tools/ (antanas@echidna.ibt.lt) Enhancement: adding a warning message to the 'cif2cod' subroutine of the COD::CIF:Data::CIF2COD module that informs the user that the data block does not contain fractional coordinates.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@4997 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/Data/CIF2COD.pm
+++ src/lib/perl5/COD/CIF/Data/CIF2COD.pm
@@ -207,8 +207,11 @@ sub cif2cod
     my $sigmas = $dataset->{precisions};
     my $dataname = $dataset->{name};
 
-    return undef unless exists $values->{_atom_site_fract_x} ||
-                 $use_datablocks_without_coord;
+    if ( !exists $values->{_atom_site_fract_x} &&
+         !$use_datablocks_without_coord ) {
+        warn "data block does not contain fractional coordinates\n";
+        return undef;
+    };
 
     my @authors = ();
     if( exists $values->{_publ_author_name} ) {
