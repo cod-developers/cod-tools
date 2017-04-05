edd0c79473a012947455442d3612e4ad51838d2c
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2017-04-05 20:20:28 +0000

    trunk/ (saulius@kolibris.saulius-grazulis.lt) Adding the 'libxml-simple-perl' as a run dependency for the Debian-8.6 systems.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5184 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Debian-8.6/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+    curl \
+    gawk \
+    libcapture-tiny-perl \
+    libcarp-assert-perl \
+    libclone-perl \
+    libdbd-mysql-perl \
+    libdbd-sqlite3-perl \
+    libdbi-perl \
+    libdigest-sha-perl \
+    libhtml-parser-perl \
+    libjson-perl \
+    libxml-simple-perl \
+    liblist-moreutils-perl \
+    libparse-yapp-perl \
+    libwww-curl-perl \
+    openbabel \
+    perl \
+    python-argparse \
+    sqlite3 \
+    tree \
+;
