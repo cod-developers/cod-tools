f8868d5e117d401fd7f6e167d07f8a0f8b985336
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-05-15 10:08:29 +0000

    trunk/ Implementing command line option '--version' in Perl language scripts. Adding a Shell test.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5251 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/check_version_option.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This Shell test checks whether all of the scripts in perl-scripts/ accept
+# '--help' command line option and prints out some useful information.
+
+#BEGIN DEPEND------------------------------------------------------------------
+
+INPUT_SCRIPTS=$(find scripts -maxdepth 1 -name \*~ -prune -o -type f -a -executable -print | sort | xargs echo)
+VERSION_FILE=.version
+
+#END DEPEND--------------------------------------------------------------------
+
+VERSION=$(grep -v ^# ${VERSION_FILE})
+VERSION_STR="cod-tools version ${VERSION}"
+
+for i in ${INPUT_SCRIPTS}
+do
+    if ./$i --version </dev/null >/dev/null 2>&1
+    then
+        ./$i --version </dev/null | diff <(echo ${VERSION_STR}) >/dev/null - ||
+        (
+            echo $i:
+            ./$i --version </dev/null | diff <(echo ${VERSION_STR}) -
+        )
+    else
+        echo $i: No --version option
+    fi
+done
