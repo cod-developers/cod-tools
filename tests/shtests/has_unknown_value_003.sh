16fb5265faaa0e6a67e44239f4c001cd7f27aa2c
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-10-19 07:52:47 +0000

    cod-tools/ (antanas@echidna) Bug fix: modifying the 'has_inapplicable_value()' and 'has_unknown_value()' subroutines in the COD::CIF::Tags::Manage module to treat values with matching values and undefined types as special.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5624 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/has_unknown_value_003.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULES='src/lib/perl5/COD/CIF/Tags/Manage.pm'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::CIF::Tags::Manage::has_unknown_value() subroutine.
+#* Tests the way the subroutine behaves when the the 'types' hash entry for
+#* the data item is undefined.
+#**
+use strict;
+use warnings;
+
+use COD::CIF::Tags::Manage;
+use COD::Serialise qw( serialiseRef );
+
+my $data_block =
+{
+  'tags'   => [ '_value' ],
+  'loops'  => [ ],
+  'inloop' => {},
+  'values' => { '_value' => [ '?' ] },
+  'precisions' => {},
+  'types' => {},
+};
+
+my $data_name   = '_value';
+
+my $unknown = COD::CIF::Tags::Manage::has_unknown_value(
+                $data_block,
+                $data_name,
+                0 );
+
+print "Value is " .($unknown ? 'unknown' : 'known') . ".\n";
+
+END_SCRIPT
