360c2923578a48e22dc04e0fc1c2cfbf5c515f3f
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-10-29 21:53:50 +0000

    cod-tools/ (antanas@kurmis) Enhancement: adding the COD::CIF::DDL::Ranges::parse_range subroutine.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5681 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/DDL/Ranges.pm
+++ src/lib/perl5/COD/CIF/DDL/Ranges.pm
@@ -18,10 +18,31 @@ use Scalar::Util qw( looks_like_number );
 require Exporter;
 our @ISA = qw( Exporter );
 our @EXPORT_OK = qw(
+    parse_range
     is_in_range
-    is_in_range_numeric
-    is_in_range_char
 );
+
+##
+# Parse a DDL range string.
+#
+# @param $range_string
+#       String specifying a range as defined in DDL.
+# @return $range
+#       A reference to an array of two element specifying the range.
+#       The first element specifies the lower bound and the second element
+#       specifies the upper bound. An undefined element signals that the
+#       associated bound is not declared. Either of the elements can be
+#       undefined.
+##
+sub parse_range
+{
+    my ($range_string) = @_;
+
+    $range_string =~ m/^([^:]+)?:([^:]+)?$/;
+    my @range = ($1, $2);
+
+    return \@range;
+}
 
 ##
 # Check value against range (defined in dictionary).
@@ -33,10 +54,9 @@ our @EXPORT_OK = qw(
 #         {
 #           'type'  => 'numb',
 #                String, representing the type of value ('numb' or 'char').
-#           'min'   => 0,
-#                The minimum range value.
-#           'max'   => 10,
-#                The maximum range value.
+#           'range' => [0, 10]
+#                Reference to range array as returned by the
+#                COD::CIF::DDL::Ranges::parse_range() subroutine.
 #           'sigma  => 0.1
 #               Standard deviation to be used when comparing numeric values
 #               (3 sigma rule). If sigma is not provided, values are compared
@@ -51,8 +71,10 @@ sub is_in_range
 {
     my ( $value, $param ) = @_;
 
-    if( !exists $param->{'min'} &&
-        !exists $param->{'max'} ) {
+    my $range = $param->{'range'};
+
+    if( !defined $range->[0] &&
+        !defined $range->[1] ) {
         return -1;
     }
 
@@ -70,10 +92,9 @@ sub is_in_range
 # @param $param
 #       Parameter hash with the following keys:
 #         {
-#           'min' - => 0,
-#                The minimum range value.
-#           'max'   => 10,
-#                The maximum range value.
+#           'range' => [0, 10]
+#                Reference to range array as returned by the
+#                COD::CIF::DDL::Ranges::parse_range() subroutine.
 #           'sigma  => 0.1
 #               Standard deviation to be used when comparing numeric values
 #               (3 sigma rule). If sigma is not provided, values are compared
@@ -88,8 +109,8 @@ sub is_in_range_numeric
 {
     my ( $value, $param ) = @_;
 
-    my $min   = $param->{'min'};
-    my $max   = $param->{'max'};
+    my $min   = $param->{'range'}[0];
+    my $max   = $param->{'range'}[1];
     my $sigma = $param->{'sigma'};
 
     if( ! looks_like_number($value) ) {
@@ -118,10 +139,9 @@ sub is_in_range_numeric
 # @param $param
 #       Parameter hash with the following keys:
 #         {
-#           'min' - => a,
-#                The minimum range value.
-#           'max'   => c,
-#                The maximum range value.
+#           'range' => [a, c]
+#                Reference to range array as returned by the
+#                COD::CIF::DDL::Ranges::parse_range() subroutine.
 #         }
 # @return
 #        0 if the value is out of the provided range;
@@ -131,10 +151,12 @@ sub is_in_range_char
 {
     my ( $value, $param ) = @_;
 
+    my $range = $param->{'range'};
+
     if(
-        ( !exists $param->{max} || $value le $param->{max} )
+        ( !exists $param->[0] || $value ge $range->[0] )
         &&
-        ( !exists $param->{min} || $value ge $param->{min} )
+        ( defined $range->[1] || $value le $range->[1] )
     ) {
         return 1;
     }
