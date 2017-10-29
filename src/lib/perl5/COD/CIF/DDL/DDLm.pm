80396b22152c0d54040ac6d6377538475bbbeee4
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-10-29 16:30:43 +0000

    branches/feature/ (antanas@kurmis) Enhancement: implementing the proper import of DDLm categories.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/antanas-DDLm-validation@5676 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/DDL/DDLm.pm
+++ src/lib/perl5/COD/CIF/DDL/DDLm.pm
@@ -24,6 +24,16 @@ our @EXPORT_OK = qw(
     merge_save_blocks
 );
 
+# From DDL dictionary version 3.13.1
+my %import_defaults = (
+    'mode' => 'Contents'
+);
+
+my %data_item_defaults = (
+    '_definition.scope' => 'Item',
+    '_definition.class' => 'Datum'
+);
+
 #
 # dictionary = {
 #   'file_path'       => [ 'dir1', 'dir2', 'dir3' ]
@@ -95,7 +105,18 @@ sub merge_imported_files
             foreach my $imported_saveblock ( @{$imported_file->{'save_blocks'}} ) {
               # TODO: check for case-sensitivity
               if ( $imported_saveblock->{'name'} eq $target_saveblock ) {
-                $saveblock = merge_save_blocks($saveblock, $imported_saveblock);
+                if (defined $imported_saveblock->{'values'}{'_definition.scope'} &&
+                    lc $imported_saveblock->{'values'}{'_definition.scope'}[0] eq 'category' ) {
+                    my $imports = get_category_imports($saveblock, $imported_file, $import );
+                    foreach( @{$imports} ) {
+                     #   print $_->{'name'};
+                     #   print "\n";
+                    }
+                  #  exit;
+                    push @{$dict->{'save_blocks'}}, @{$imports};
+                } else {
+                    $saveblock = merge_save_blocks($saveblock, $imported_saveblock);
+                }
               }
             }
           }
@@ -104,6 +125,121 @@ sub merge_imported_files
     }
 
     return $dict;
+}
+
+sub get_category_imports
+{
+    my ($save_block, $import_data, $import_options) = @_;
+
+    my $parent_block_scope =
+        defined $save_block->{'values'}{'_definition.scope'} ?
+                $save_block->{'values'}{'_definition.scope'}[0] :
+                $data_item_defaults{'_definition.scope'};
+
+    if ( lc $parent_block_scope ne 'category' ) {
+        die "ERROR, a category import '$import_options->{'save'}' from file " .
+            "'$import_options->{'file'}' defined in a non-category save block " .
+            "'$save_block->{'name'}'\n";
+    }
+
+    my $import_block_id = uc $import_options->{'save'};
+    my $import_block;
+    for my $block ( @{$import_data->{'save_blocks'}} ) {
+        if ( uc $block->{'values'}{'_definition.id'}[0] eq $import_block_id ) {
+            $import_block = $block;
+            last;
+        }
+    }
+
+    # Head category importing a head category is a special case
+    my $parent_block_class =
+        defined $save_block->{'values'}{'_definition.class'} ?
+                $save_block->{'values'}{'_definition.class'}[0] :
+                $data_item_defaults{'_definition.class'};
+    my $import_block_class =
+        defined $import_block->{'values'}{'_definition.class'} ?
+                $import_block->{'values'}{'_definition.class'}[0] :
+                $data_item_defaults{'_definition.class'};
+    my $head_in_head = lc $parent_block_class eq 'head' &&
+                       lc $import_block_class eq 'head';
+
+    my $import_type = defined $import_options->{'mode'} ?
+                              $import_options->{'mode'} :
+                              $import_defaults{'mode'};
+
+    # TODO: warn about a import type mismatch
+    if ( $head_in_head ) {
+        $import_type = 'full';
+    }
+
+    my $imported_save_blocks = get_child_blocks(
+        $import_block_id,
+        $import_data,
+        {
+         'recursive' => ( lc $import_type eq 'full' )
+        }
+    );
+
+    my $parent_block_id = $save_block->{'values'}{'_definition.id'}[0];
+    if ( $head_in_head ) {
+        for my $block (@{$imported_save_blocks}) {
+            if ( uc $block->{'values'}{'_name.category_id'}[0] eq $import_block_id ) {
+                $block->{'values'}{'_name.category_id'}[0] = $parent_block_id;
+            }
+        }
+    } else {
+        $import_block->{'values'}{'_name.category_id'}[0] =
+            $save_block->{'values'}{'_definition.id'}[0];
+        push @{$imported_save_blocks}, $parent_block_id;
+    }
+
+    return $imported_save_blocks;
+}
+
+##
+# Selects blocks that balong to 
+#
+# @param $id
+#       Indentifier name of the category. The identifier is usually stored as
+#       the value of the '_definition.id' data item.
+# @param $data
+#       Reference to a data frame as returned by the COD::CIF::Parser.
+# @param $options
+#       Reference to a hash of options. The following options are recognised:
+#       {
+#       # Recursively select child blocks.
+#           'recursive' => 0
+#       }
+# @return
+#       Reference to an array of child blocks.
+##
+sub get_child_blocks
+{
+    my ($id, $data, $options) = @_;
+
+    my $recursive = defined $options->{'recursive'} ?
+                            $options->{'recursive'} : 0;
+
+    my @blocks;
+    $id = uc $id;
+    for my $block ( @{$data->{'save_blocks'}} ) {
+        my $block_id       = uc $block->{'values'}{'_definition.id'}[0];
+        my $block_category = uc $block->{'values'}{'_name.category_id'}[0];
+        my $block_scope    =
+            defined $block->{'values'}{'_definition.scope'} ?
+                (uc $block->{'values'}{'_definition.scope'}[0] ) :
+                $data_item_defaults{'_definition.scope'};
+
+        if ( $block_category eq $id ) {
+            push @blocks, $block;
+            if ( lc $block_scope eq 'category' && $recursive ) {
+                push @blocks,
+                     @{ get_child_blocks($block_id, $data, $options ) };
+            }
+        }
+    }
+
+    return \@blocks;
 }
 
 # TODO: rewrite as non-destructive?
