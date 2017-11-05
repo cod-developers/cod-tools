5ea03f6a6b1bd20262e20ae2cd317d9d47f0075d
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-11-05 02:44:44 +0000

    cod-tools/ (antanas@kurmis) Refactoring: moving the datetime parsing functionality into a separate subroutine in the COD::CIF::Data::Check module in order for it to be reused more easily.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5712 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/parse_datetime_001.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULES='src/lib/perl5/COD/CIF/Data/Check.pm'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::CIF::Data::Check::parse_datetime() subroutine.
+#* Tests how various timestamp strings that correspond to the required
+#* format are handled.
+#**
+
+use strict;
+use warnings;
+
+use COD::CIF::Data::Check;
+
+my @datetime_values = (
+    # Date-only timestamp
+    '2000-01-01',
+    # Examples from RFC 3339
+    '1985-04-12T23:20:50.52Z',
+    '1990-12-31T23:59:60Z',
+    '1990-12-31T15:59:60-08:00',
+    '1937-01-01T12:00:27.87+00:20',
+    # Lower case letters
+    '1985-04-12t23:20:50.52z',
+);
+
+for (@datetime_values) {
+    my $dt = COD::CIF::Data::Check::parse_datetime($_);
+    print $dt->datetime . "\n";
+}
+
+END_SCRIPT
