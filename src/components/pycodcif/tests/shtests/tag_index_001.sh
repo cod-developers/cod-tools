f2adf1e7aeaeecd1e73d7eb432b3fa2bd17c69cc
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-11-08 14:45:37 +0000

    trunk/src/components/pycodcif/ Wrapping two more functions of codcif: -- new_value_from_scalar() (from cifvalue.h), -- datablock_overwrite_cifvalue() (from datablock.h).
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5732 04be6746-3802-0410-999d-98508da1e98c

--- src/components/pycodcif/tests/shtests/tag_index_001.sh
+++ src/components/pycodcif/tests/shtests/tag_index_001.sh
@@ -11,9 +11,12 @@
 #  Test driver for pycodcif module.
 #**
 import sys
-from pycodcif import new_cif_from_cif_file, datablock_tag_index, cif_option_default, cif_datablock_list
+from pycodcif import new_cif_from_cif_file, new_value_from_scalar, datablock_tag_index, datablock_overwrite_cifvalue, cif_option_default, cif_datablock_list
 
 cif = new_cif_from_cif_file( "tests/inputs/1YGG.cif", cif_option_default(), None )
 datablock = cif_datablock_list( cif )
 print datablock_tag_index( datablock, "_audit_author.name" )
 print datablock_tag_index( datablock, "_this_tag_does_not_exist" )
+
+cifvalue = new_value_from_scalar( "Someone Else", "CIF_UQSTRING", None )
+datablock_overwrite_cifvalue( datablock, 22, 0, cifvalue, None )
