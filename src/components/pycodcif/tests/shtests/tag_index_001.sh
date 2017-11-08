51a1e46a2c0ef99a0dd6c7fadb85cb2fcbbbd9db
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-11-08 15:00:11 +0000

    trunk/src/components/pycodcif/ Wrapping two more functions of codcif: -- value_dump() (from cifvalue.h), -- datablock_cifvalue() (from datablock.h).
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@5733 04be6746-3802-0410-999d-98508da1e98c

--- src/components/pycodcif/tests/shtests/tag_index_001.sh
+++ src/components/pycodcif/tests/shtests/tag_index_001.sh
@@ -11,12 +11,18 @@
 #  Test driver for pycodcif module.
 #**
 import sys
-from pycodcif import new_cif_from_cif_file, new_value_from_scalar, datablock_tag_index, datablock_overwrite_cifvalue, cif_option_default, cif_datablock_list
+from pycodcif import new_cif_from_cif_file, new_value_from_scalar, datablock_tag_index, datablock_overwrite_cifvalue, cif_option_default, cif_datablock_list, value_dump, datablock_cifvalue
 
 cif = new_cif_from_cif_file( "tests/inputs/1YGG.cif", cif_option_default(), None )
 datablock = cif_datablock_list( cif )
 print datablock_tag_index( datablock, "_audit_author.name" )
 print datablock_tag_index( datablock, "_this_tag_does_not_exist" )
 
+# Overwrites an existing value in the datablock
 cifvalue = new_value_from_scalar( "Someone Else", "CIF_UQSTRING", None )
 datablock_overwrite_cifvalue( datablock, 22, 0, cifvalue, None )
+
+# Checks whether the value is correctly overwritten
+cifvalue = datablock_cifvalue( datablock, 22, 0 )
+value_dump( cifvalue )
+print ""
