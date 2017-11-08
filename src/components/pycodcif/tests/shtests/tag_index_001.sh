75ce2855f2126f39cddea96f647d0207b46cd7f0
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-11-08 15:06:59 +0000

    trunk/src/components/pycodcif/ Wrapping datablock_insert_cifvalue() from datablock.h.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5734 04be6746-3802-0410-999d-98508da1e98c

--- src/components/pycodcif/tests/shtests/tag_index_001.sh
+++ src/components/pycodcif/tests/shtests/tag_index_001.sh
@@ -11,12 +11,22 @@
 #  Test driver for pycodcif module.
 #**
 import sys
-from pycodcif import new_cif_from_cif_file, new_value_from_scalar, datablock_tag_index, datablock_overwrite_cifvalue, cif_option_default, cif_datablock_list, value_dump, datablock_cifvalue
+from pycodcif import new_cif_from_cif_file, new_value_from_scalar, datablock_tag_index, datablock_overwrite_cifvalue, datablock_insert_cifvalue, cif_option_default, cif_datablock_list, value_dump, datablock_cifvalue
 
 cif = new_cif_from_cif_file( "tests/inputs/1YGG.cif", cif_option_default(), None )
 datablock = cif_datablock_list( cif )
 print datablock_tag_index( datablock, "_audit_author.name" )
 print datablock_tag_index( datablock, "_this_tag_does_not_exist" )
+
+# Inserts non-existing data item in the datablock
+cifvalue = new_value_from_scalar( "12.3456789", "CIF_FLOAT", None )
+datablock_insert_cifvalue( datablock, "_this_tag_does_not_exist", cifvalue, None )
+
+# Checks whether the value is correctly inserted
+tag_id = datablock_tag_index( datablock, "_this_tag_does_not_exist" )
+cifvalue = datablock_cifvalue( datablock, tag_id, 0 )
+value_dump( cifvalue )
+print ""
 
 # Overwrites an existing value in the datablock
 cifvalue = new_value_from_scalar( "Someone Else", "CIF_UQSTRING", None )
