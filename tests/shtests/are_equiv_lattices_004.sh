86e9d51424b6c9148ef8aa64e6982db809b0e97a
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-11-09 23:55:49 +0000

    cod-tools/ (antanas@kurmis) Enhancement: adding 'are_equiv_lattices' to the list of exported subroutines in COD::CIF::Data::CODNumbers module.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@5754 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/are_equiv_lattices_004.sh
@@ -0,0 +1,77 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULES='src/lib/perl5/COD/CIF/Data/CODNumbers.pm'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::CIF::Data::CODNumbers::are_equiv_lattices()
+#* subroutine. Tests the way the subroutine behaves when two lattices
+#* with differing cell lengths are compared. In this case the s.u.
+#* values are provided and the 'use_su' option is enabled. The lattices
+#* should be considered equivalent.
+#**
+
+use strict;
+use warnings;
+
+use COD::CIF::Data::CODNumbers;
+
+my $entry_1 = {
+    'cell' => {
+        '_cell_length_a'    => 5,
+        '_cell_length_b'    => 5,
+        '_cell_length_c'    => 5,
+        '_cell_angle_alpha' => 90,
+        '_cell_angle_beta'  => 90,
+        '_cell_angle_gamma' => 90,
+    },
+    'sigcell' => {
+        '_cell_length_a'    => 2,
+        '_cell_length_b'    => 2,
+        '_cell_length_c'    => 2,
+        '_cell_angle_alpha' => undef,
+        '_cell_angle_beta'  => undef,
+        '_cell_angle_gamma' => undef,
+    },
+};
+
+my $entry_2 = {
+    'cell' => {
+        '_cell_length_a'    => 8,
+        '_cell_length_b'    => 8,
+        '_cell_length_c'    => 8,
+        '_cell_angle_alpha' => 90,
+        '_cell_angle_beta'  => 90,
+        '_cell_angle_gamma' => 90,
+    },
+    'sigcell' => {
+        '_cell_length_a'    => 2,
+        '_cell_length_b'    => 2,
+        '_cell_length_c'    => 2,
+        '_cell_angle_alpha' => undef,
+        '_cell_angle_beta'  => undef,
+        '_cell_angle_gamma' => undef,
+    },
+};
+
+my $equivalent = COD::CIF::Data::CODNumbers::are_equiv_lattices(
+    $entry_1,
+    $entry_2,
+    { 'use_su' => 1 }
+);
+
+if ( $equivalent ) {
+    print "Lattices were treated as equivalent.\n";
+} else {
+    print "Lattices were treated as not equivalent.\n";
+}
+
+END_SCRIPT
---
+++ tests/shtests/are_equiv_lattices_006.sh
@@ -0,0 +1,81 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULES='src/lib/perl5/COD/CIF/Data/CODNumbers.pm'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::CIF::Data::CODNumbers::are_equiv_lattices()
+#* subroutine. Tests the way the subroutine behaves when two lattices
+#* with differing cell parameters are compared. In this case the s.u.
+#* values are not provided and the 'max_cell_length_diff' and
+#* 'max_cell_angle_diff' options are given explicit values. The lattices
+#* should be considered equivalent.
+#**
+
+use strict;
+use warnings;
+
+use COD::CIF::Data::CODNumbers;
+
+my $entry_1 = {
+    'cell' => {
+        '_cell_length_a'    => 5,
+        '_cell_length_b'    => 5,
+        '_cell_length_c'    => 5,
+        '_cell_angle_alpha' => 90,
+        '_cell_angle_beta'  => 90,
+        '_cell_angle_gamma' => 90,
+    },
+    'sigcell' => {
+        '_cell_length_a'    => undef,
+        '_cell_length_b'    => undef,
+        '_cell_length_c'    => undef,
+        '_cell_angle_alpha' => undef,
+        '_cell_angle_beta'  => undef,
+        '_cell_angle_gamma' => undef,
+    },
+};
+
+my $entry_2 = {
+    'cell' => {
+        '_cell_length_a'    => 4.5,
+        '_cell_length_b'    => 5,
+        '_cell_length_c'    => 5,
+        '_cell_angle_alpha' => 85,
+        '_cell_angle_beta'  => 90,
+        '_cell_angle_gamma' => 90,
+    },
+    'sigcell' => {
+        '_cell_length_a'    => undef,
+        '_cell_length_b'    => undef,
+        '_cell_length_c'    => undef,
+        '_cell_angle_alpha' => undef,
+        '_cell_angle_beta'  => undef,
+        '_cell_angle_gamma' => undef,
+    },
+};
+
+my $equivalent = COD::CIF::Data::CODNumbers::are_equiv_lattices(
+    $entry_1,
+    $entry_2,
+    {
+        'max_cell_length_diff' => '0.6',
+        'max_cell_angle_diff'  => '6',
+    }
+);
+
+if ( $equivalent ) {
+    print "Lattices were treated as equivalent.\n";
+} else {
+    print "Lattices were treated as not equivalent.\n";
+}
+
+END_SCRIPT
