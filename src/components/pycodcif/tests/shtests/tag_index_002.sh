1624b5c44ed2cbda175772321b7d4dfb727eef08
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2017-11-10 09:44:07 +0000

    trunk/src/components/pycodcif/ Implementing CifFile.__str__() method. Wrapping cif_append_datablock() of cif.h.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@5761 04be6746-3802-0410-999d-98508da1e98c

--- src/components/pycodcif/tests/shtests/tag_index_002.sh
+++ src/components/pycodcif/tests/shtests/tag_index_002.sh
@@ -11,9 +11,13 @@
 #  Test driver for pycodcif module.
 #**
 import sys
-from pycodcif import CifDatablock
+from pycodcif import CifFile, CifDatablock
 
 datablock = CifDatablock("new")
 datablock['_tag'] = 10
 
 print datablock['_tag']
+
+cif = CifFile()
+cif.append( datablock )
+print cif
