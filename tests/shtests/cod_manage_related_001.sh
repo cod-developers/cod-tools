b566ccc611b51e2f4825abe606b970a162c7e4fc
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2017-11-14 13:29:56 +0000

    branches/feature/ (antanas@echidna) Enhancement: adding a few more tests for the 'cod_manage_related' script.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/antanas-cod_manage_related@5802 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/cod_manage_related_001.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Tests the way the 'cod_manage_related' script handles reading from STDIN.
+#**
+
+#BEGIN DEPEND------------------------------------------------------------------
+
+INPUT_SCRIPT=scripts/cod_manage_related
+INPUT_CIF=tests/inputs/related_empty_entry.cif
+
+#END DEPEND--------------------------------------------------------------------
+
+set -ue
+
+PATH=.:${PATH}
+
+cat ${INPUT_CIF} | ${INPUT_SCRIPT} \
+--related-entry-database    "COD on a ROD" \
+--related-entry-code        "F15H" \
+--related-entry-description "It might struggle!" \
+--related-entry-uri         "http://www.fishonastick.com"
