7313ad36c9e2fed98155dfef2564255b1944dbf2
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2018-05-25 03:05:43 +0000

    trunk/ (antanas@kurmis) Enhancement: adding the COD::CIF::Tags::Manage::has_numeric_value() subroutine.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@6185 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/has_numeric_value_001.sh
@@ -0,0 +1,63 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULES='src/lib/perl5/COD/CIF/Tags/Manage.pm'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::CIF::Tags::Manage::has_numeric_value() subroutine.
+#* Tests the way the subroutine considers the value of the 'values' hash.
+#**
+
+use strict;
+use warnings;
+
+use COD::CIF::Tags::Manage;
+use COD::Serialise qw( serialiseRef );
+
+##
+#data_test
+#loop_
+#_value
+#text
+#5
+#5(5)
+#5.5
+#5.5(5)
+#'12'
+#"12(5)"
+#;42
+#;
+##
+my $data_block =
+{
+  'tags'   => [ '_value' ],
+  'loops'  => [ ],
+  'inloop' => {},
+  'values' => { '_value' => [ 'text', '5', '5(5)', '5.5', '5.5(5)', 12, '12(5)', 42, 30 ] },
+  'precisions' => {},
+  'types'  => {
+    '_value' => [ 'UQSTRING', 'INT', 'INT', 'FLOAT', 'FLOAT', 'SQSTRING', 'DQSTRING', 'TEXTFIELD', undef ]
+   },
+};
+
+my $data_name = '_value';
+
+for (my $i = 0; $i < @{$data_block->{'values'}{$data_name}}; $i++) {
+    my $numeric = COD::CIF::Tags::Manage::has_numeric_value(
+                $data_block,
+                $data_name,
+                $i
+    );
+    print "Value at position $i is " .
+           ($numeric ? 'numeric' : 'not numeric') . ".\n";
+}
+
+END_SCRIPT
