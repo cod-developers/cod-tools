698bf5606f798efecbb632cddbfec95103bd496d
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2018-06-04 01:57:53 +0000

    trunk/ Using value of ${TMP_DIR} (if defined) for temporary directory creation in tests/shtests/cif_find_duplicates_006.sh.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@6212 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/cif_find_duplicates_006.sh
+++ tests/shtests/cif_find_duplicates_006.sh
@@ -14,8 +14,10 @@ unset LC_CTYPE
 find_numbers=${INPUT_SCRIPT}
 
 BASENAME="`basename $0 .sh`"
-TMP_DIR="./tmp-${BASENAME}"
-mkdir ${TMP_DIR}
+
+test -z "${TMP_DIR}" && TMP_DIR="."
+TMP_DIR="${TMP_DIR}/tmp-${BASENAME}"
+mkdir "${TMP_DIR}"
 
 TMP_OUT="${TMP_DIR}/$(basename ${find_numbers}).out"
 TMP_ERR="${TMP_DIR}/$(basename ${find_numbers}).err"
@@ -25,7 +27,7 @@ ${find_numbers} --continue-on-errors \
     2> ${TMP_ERR} \
     | sort > ${TMP_OUT}
 
-cat ${TMP_OUT}
-cat ${TMP_ERR}
+cat "${TMP_OUT}"
+cat "${TMP_ERR}"
 
-rm -rf ${TMP_DIR}
+rm -rf "${TMP_DIR}"
