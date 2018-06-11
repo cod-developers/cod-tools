37ff7b66f04f782a5e9571f509b6e18c95271601
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2018-06-11 03:16:25 +0000

    cod-tools/ (antanas@echidna) Enhancement: adding build and run dependencies for Fedora-28.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@6225 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Fedora-28/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo dnf install -y \
+    openbabel \
+    perl-Capture-Tiny \
+    perl-Carp-Assert \
+    perl-Clone \
+    perl-Date-Calc \
+    perl-DateTime-Format-RFC3339 \
+    perl-DBD-MySQL \
+    perl-DBD-SQLite \
+    perl-Digest-SHA \
+    perl-JSON \
+    perl-List-MoreUtils \
+    perl-open
+    perl-openbabel \
+    perl-WWW-Curl \
+    perl-XML-Simple
+
