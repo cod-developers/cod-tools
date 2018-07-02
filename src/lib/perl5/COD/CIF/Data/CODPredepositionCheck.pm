b6c40bc1d191287230b10a8b3718506d396af371
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2018-07-02 12:35:08 +0000

    cod-tools/ (antanas@echidna) Refactoring: modified the warning message about missing authors in the cif_cod_check to use the same format as other warnings about missing data items. Modified the COD::CIF::Data::Check::check_author_names() subroutine to no longer raise warnings about missing _publ_author_name data items.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@6270 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/Data/CODPredepositionCheck.pm
+++ src/lib/perl5/COD/CIF/Data/CODPredepositionCheck.pm
@@ -244,7 +244,9 @@ sub filter_and_check
                         if( $deposition_type ne 'published' &&
                             ( /(_journal_name_full|_publ_author_name) is undefined/ ||
                               /neither _journal_year nor _journal_volume is defined/ ||
-                              /neither _journal_page_first nor _journal_article_reference is defined/ ) ) {
+                              /neither _journal_page_first nor _journal_article_reference is defined/ ||
+                              /recommended data item '_publ_author_name' was not found/
+                         ) ) {
                             next CCCMESSAGE;
                         }
                         if( !defined $parsed->{err_level} ) {
