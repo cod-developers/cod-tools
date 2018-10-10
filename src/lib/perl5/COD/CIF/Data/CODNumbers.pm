dba66ef5e2b6124068af21f979fe21f3c929c944
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2018-10-10 14:23:27 +0000

    cod-tools/ (antanas@echidna) Bug fix: updated the error handling logic in the COD::Formulae::Parser::AdHoc module to raise the warn signal instead of printing directly to STDERR.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@6464 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/Data/CODNumbers.pm
+++ src/lib/perl5/COD/CIF/Data/CODNumbers.pm
@@ -246,25 +246,25 @@ sub cif_fill_data
 
             my $formula_parser = COD::Formulae::Parser::AdHoc->new();
 
+            my @formula_parser_warnings;
             eval {
+                local $SIG{__WARN__} = sub {
+                    push @formula_parser_warnings, @_;
+                };
                 $formula_parser->ParseString( $formula );
-                if( defined $formula_parser->YYData->{ERRCOUNT} &&
-                    $formula_parser->YYData->{ERRCOUNT} > 0 ) {
-                    die "ERROR, $formula_parser->YYData->{ERRCOUNT} "
-                      . 'error(s) encountered while parsing chemical '
-                      . "formula sum\n";
-                    ;
-                } else {
-                    $formula = $formula_parser->SprintFormula;
-                }
             };
-            if( $@ ) {
+            if ( !@formula_parser_warnings ) {
+                $formula = $formula_parser->SprintFormula;
+            } else {
+                for( @formula_parser_warnings ) {
+                    print STDERR $_;
+                }
                 warn "WARNING, could not parse formula '$formula' "
                    . "resorting to simple split routine\n";
                 $formula = join ' ', sort {$a cmp $b} split( ' ', $formula );
             }
         }
-            $structure{chemical_formula_sum} = $formula;
+        $structure{chemical_formula_sum} = $formula;
     }
 
     my $calc_formula;
