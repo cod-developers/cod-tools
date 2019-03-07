912a51bbec861d12a335f9f923b5dd6dd6d42841
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2019-03-07 11:27:21 +0000

    branches/feature/andrius-DDLm-handling/ Adding COD::CIF::DDL module with get_cif_dictionary_ids() function.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/andrius-DDLm-handling@6742 04be6746-3802-0410-999d-98508da1e98c

---
+++ src/lib/perl5/COD/CIF/DDL.pm
@@ -0,0 +1,55 @@
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* A set of generic subroutines for DDL handling.
+#**
+
+package COD::CIF::DDL;
+
+use strict;
+use warnings;
+
+use COD::CIF::Tags::Manage qw( cifversion );
+
+require Exporter;
+our @ISA = qw( Exporter );
+our @EXPORT_OK = qw(
+    get_cif_dictionary_ids
+);
+
+sub get_cif_dictionary_ids
+{
+    my( $datablock ) = @_;
+
+    my $dictionary_tags = {
+        1.1 => { name    => '_audit_conform_dict_name',
+                 version => '_audit_conform_dict_version',
+                 uri     => '_audit_conform_dict_location' },
+        2.0 => { name    => '_dictionary.title',
+                 version => '_dictionary.version',
+                 uri     => '_dictionary.uri' }
+    };
+
+    my $cifversion = cifversion( $datablock );
+    $cifversion = 1.1 unless $cifversion;
+
+    my @tags_in_cif = grep { exists $datablock->{values}{$_} }
+                           sort values %{$dictionary_tags->{$cifversion}};
+
+    return if !@tags_in_cif;
+
+    my @dictionaries;
+    for my $i (0..$#{$datablock->{values}{$tags_in_cif[0]}}) {
+        push @dictionaries,
+             { map { $_ => $datablock->{values}{$dictionary_tags->{$cifversion}{$_}}[$i] }
+                   keys %{$dictionary_tags->{$cifversion}} };
+    }
+
+    return @dictionaries;
+}
+
+1;
