7131a7ca8959926aa3028e9548293c240005e188
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2019-04-08 09:33:10 +0000

    cod-tools/ (antanas@echidna) Changed: updated several warning messages in the 'cif_cod_check' script and the COD::CIF::Data module.
    git-svn-id: svn://cod.ibt.lt/cod-tools/trunk@6902 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/Data.pm
+++ src/lib/perl5/COD/CIF/Data.pm
@@ -76,7 +76,7 @@ sub get_cell
         } elsif( $options->{silent} ) {
             push(@cell_lengths_and_angles, undef);
         } else {
-            die "ERROR, cell length data item '$cif_tag' not present" . "\n";
+            die "ERROR, cell length data item '$cif_tag' was not found" . "\n";
         }
     }
 
@@ -92,7 +92,7 @@ sub get_cell
         } elsif( $options->{silent} ) {
             push(@cell_lengths_and_angles, undef);
         } else {
-            warn( "WARNING, cell angle data item '$cif_tag' not present -- "
+            warn( "WARNING, cell angle data item '$cif_tag' was not found -- "
                 . "taking default value 90 degrees\n" );
             push( @cell_lengths_and_angles, 90 );
         }
