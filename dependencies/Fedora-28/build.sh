001a05e7dd39f037b8b3dafdb1c5afa1fffd776e
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2019-11-27 12:49:23 +0000

    trunk/ Dropping dependency on no longer used 'flex' package.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@7503 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Fedora-28/build.sh
+++ dependencies/Fedora-28/build.sh
@@ -3,7 +3,7 @@
 sudo dnf groupinstall -y "Development Tools"
 sudo dnf install -y cmake
 sudo dnf install -y perl-Module-ScanDeps
-sudo dnf install -y perl-Parse-Yapp bison flex
+sudo dnf install -y perl-Parse-Yapp bison
 sudo dnf install -y python2-devel
 sudo dnf install -y redhat-rpm-config
 sudo dnf install -y swig
