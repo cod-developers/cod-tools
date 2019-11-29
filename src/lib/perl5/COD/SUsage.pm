1f8dbe6654c145651589069bbf63a8923060b2c5
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2019-11-29 11:26:13 +0000

    trunk/ (antanas@echidna) Added: several error messages informing about non-zero exit status while opening or closing files.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@7515 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/SUsage.pm
+++ src/lib/perl5/COD/SUsage.pm
@@ -25,7 +25,9 @@ sub usage
     my $script = shift;
     $script = $0 unless defined $script;
 
-    open my $script_fh, $script or die "Could not open $script: $!";
+    open my $script_fh, $script or
+        die "$0: $script: ERROR, could not open the '$script' file for " .
+            "reading -- " . lcfirst($!) . '.' . "\n";
     while( <$script_fh> ) {
         if( /^\s*#\*/ .. /^\s*#\*\*/ ) {
             /^\s*#\*?\*?/;
@@ -34,7 +36,9 @@ sub usage
             print $line;
         }
     }
-    close $script_fh;
+    close $script_fh or
+        die "$0: $script: ERROR, error occurred while closing the '$script' " .
+            "file after reading -- " . lcfirst($!) . '.' . "\n";
 
     return;
 }
@@ -46,7 +50,9 @@ sub options
 
     print "$script: The '--options' option is a placehoder.\n";
     print "$script: It should be replaced by one of the following options:\n";
-    open my $script_fh, $script or die $!;
+    open my $script_fh, $script or
+        die "$0: $script: ERROR, could not open the '$script' file for " .
+            "reading -- " . lcfirst($!) . '.' . "\n";
     while( <$script_fh> ) {
         if( /^#\*\s+OPTIONS:/../^#\*\*/ ) {
             s/^#\*\s+OPTIONS://;
@@ -55,7 +61,9 @@ sub options
             print;
         }
     }
-    close $script_fh;
+    close $script_fh or
+        die "$0: $script: ERROR, error occurred while closing the '$script' " .
+            "file after reading -- " . lcfirst($!) . '.' . "\n";
 
     return;
 }
