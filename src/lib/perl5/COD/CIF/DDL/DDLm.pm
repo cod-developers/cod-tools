9bd003561aff7f328dff5657e3cdda1e9578ec21
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2020-02-17 06:44:26 +0000

    branches/feature/ (antanas@echidna) Changed: fixed a typo ('collectivelly'->'collectively') in a validation message in the COD::CIF::DDL::DDLm module.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/antanas-DDLm-validation@7736 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/DDL/DDLm.pm
+++ src/lib/perl5/COD/CIF/DDL/DDLm.pm
@@ -3617,7 +3617,7 @@ sub check_composite_key_uniqueness
                         'data items [' .
                         ( join ', ', map { "'$_'" } @{$data_names} ) .
                         '] act as a composite loop key, but the associated ' .
-                        'data values are not collectivelly unique -- values [' .
+                        'data values are not collectively unique -- values [' .
                         ( join ', ', map { "'$_'" } split /$join_char/, $key ) .
                         '] appear ' .
                         ( scalar @{$unique_values{$key}} ) . ' times as ' .
