ab02c386c71b0dfcfa232f3d927ac8bd4541316b
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-05-28 03:54:17 +0000

    branches/feature/andrius-DDLm-handling/ Moving tools/ddl1-to-ddlm to scripts/. Test case tests/shtests/check_version_option.sh fails due to unimplemented --version command line option. Will fix this soon.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/andrius-DDLm-handling@7998 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/ddl1-to-ddlm_001.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#BEGIN DEPEND------------------------------------------------------------------
-INPUT_SCRIPT=tools/ddl1-to-ddlm
-INPUT_DIC=tests/inputs/cif_cod.dic
-#END DEPEND--------------------------------------------------------------------
-
-${INPUT_SCRIPT} --keep-original-date ${INPUT_DIC}
