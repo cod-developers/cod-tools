79ec1138c941390278a592bc264697d284fa83cf
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2020-06-18 18:59:47 +0000

    branches/feature/saulius-correct-polymer-basis-determination/ (saulius@varanas.saulius-grazulis.lt) Deleting the 'gj_rational_elimination_008' test since it was incorrect and when corrected takes too long to complete.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/saulius-correct-polymer-basis-determination@8044 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/gj_rational_elimination_008.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#BEGIN DEPEND------------------------------------------------------------------
-INPUT_MATRIX=tests/inputs/matrices/random-linear-equation-system3.dat
-INPUT_TEST_SCRIPT=tests/scripts/gj_elimination
-#END DEPEND--------------------------------------------------------------------
-
-"${INPUT_TEST_SCRIPT}" "${INPUT_MATRIX}"
