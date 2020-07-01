142fb359370443be6c7d781c5d592912a7322ed8
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-07-01 14:30:09 +0000

    trunk/ Adding a test demonstrating the issue of cif_cod_numbers not using the numbers of attached hydrogens (https://projects.ibt.lt/repositories/issues/747).
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8089 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/cif_cod_numbers_003.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+
+INPUT_CIF_COD_NUMBERS=scripts/cif_cod_numbers
+INPUT_CIF_SELECT=scripts/cif_select
+INPUT_CIF=tests/inputs/2000135.cif
+
+#END DEPEND--------------------------------------------------------------------
+
+set -ue
+
+${INPUT_CIF_SELECT} \
+    --tags _chemical_formula_sum \
+    --invert \
+    ${INPUT_CIF} \
+    | ${INPUT_CIF_COD_NUMBERS}
