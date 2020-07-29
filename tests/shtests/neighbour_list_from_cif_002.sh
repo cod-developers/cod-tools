a80be09a603c07b84e9da208dc592eb7bf94dc8c
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-07-29 13:30:29 +0000

    trunk/ Adding one more test case for COD::AtomNeighbours::neighbour_list_from_cif().
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8256 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/neighbour_list_from_cif_002.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_PARSER='src/lib/perl5/COD/CIF/Parser.pm'
+INPUT_NEIGHBOURS_MODULE='src/lib/perl5/COD/AtomNeighbours.pm'
+INPUT_CIF='tests/inputs/2103766.cif'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::AtomNeighbours::neighbour_list_from_cif() subroutine.
+#**
+
+use strict;
+use warnings;
+
+use COD::AtomNeighbours qw( neighbour_list_from_cif );
+use COD::CIF::Parser qw( parse_cif );
+use Data::Dumper;
+
+$Data::Dumper::Sortkeys = 1;
+
+my( $data, $dataset );
+
+( $data ) = parse_cif( 'tests/inputs/2103766.cif' );
+( $dataset ) = @$data;
+
+my $neighbour_list = neighbour_list_from_cif( $dataset );
+print Dumper $neighbour_list->{neighbours};
+
+END_SCRIPT
