d875549a3218519e1de163776835124f1732e3cd
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-08-17 12:14:06 +0000

    trunk/ Ignoring scripts with syntax errors in tests/shtests/check_help_option.sh.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8346 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/check_help_option.sh
+++ tests/shtests/check_help_option.sh
@@ -11,12 +11,15 @@ INPUT_SCRIPTS=$(find scripts -maxdepth 1 -name \*~ -prune -o -type f -a -executa
 
 for i in ${INPUT_SCRIPTS}
 do
-    HELP_MESSAGE=$(./$i --help </dev/null)
-    if [ -n "${HELP_MESSAGE}" ]
+    if perl -c ./$i 2>/dev/null
     then
-        echo "${HELP_MESSAGE}" | grep -q USAGE || echo "$i: No USAGE section"
-        echo "${HELP_MESSAGE}" | grep -q OPTIONS || echo "$i: No OPTIONS section"
-    else
-        echo "$i: No --help message output"
+        HELP_MESSAGE=$(./$i --help </dev/null)
+        if [ -n "${HELP_MESSAGE}" ]
+        then
+            echo "${HELP_MESSAGE}" | grep -q USAGE || echo "$i: No USAGE section"
+            echo "${HELP_MESSAGE}" | grep -q OPTIONS || echo "$i: No OPTIONS section"
+        else
+            echo "$i: No --help message output"
+        fi
     fi
 done
