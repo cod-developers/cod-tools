19996fe6fda87ad415836c8cda5e1e5be1fb871d
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-08-26 09:06:36 +0000

    trunk/ Extending tests in shtests/is_atom_excludable_001.sh.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8423 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/is_atom_excludable_001.sh
+++ tests/shtests/is_atom_excludable_001.sh
@@ -25,13 +25,21 @@ my $values_1 =
     _atom_site_label => [ 'H', 'h', 'HO', 'Ho', 'H1', 'h1' ],
 };
 
-for my $i (0..$#{$values_1->{_atom_site_label}}) {
-    print "\t" if $i;
-    print COD::CIF::Data::AtomList::is_atom_excludable( $values_1,
-                                                        $i,
-                                                        { is_hydrogen => 1 } )
-          ? 'YES' : 'NO';
+my $values_2 =
+{
+    _atom_site_label       => [ 'H',  'h', 'HO', 'Ho', 'H1', 'h1' ],
+    _atom_site_type_symbol => [ 'He', 'D', 'H',  'h',  '?' ],
+};
+
+for my $values ($values_1, $values_2) {
+    for my $i (0..$#{$values->{_atom_site_label}}) {
+        print "\t" if $i;
+        print COD::CIF::Data::AtomList::is_atom_excludable( $values,
+                                                            $i,
+                                                            { is_hydrogen => 1 } )
+              ? 'YES' : 'NO';
+    }
+    print "\n";
 }
-print "\n";
 
 END_SCRIPT
