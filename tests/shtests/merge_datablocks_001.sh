7bbfb35aaa5ee704d2d94f733206b31654e5235b
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-09-09 11:27:36 +0000

    trunk/ Adding a pair of tests for COD::CIF::Tags::Merge::merge_datablocks().
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8476 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/merge_datablocks_001.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULES='src/lib/perl5/COD/CIF/Tags/Merge.pm'
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::CIF::Tags::Merge::merge_datablocks() subroutine.
+#* Tests overwriting a looped data item with non-looped one.
+#**
+
+use strict;
+use warnings;
+
+use COD::CIF::Tags::Manage qw( new_datablock set_loop_tag set_tag );
+use COD::CIF::Tags::Merge qw( merge_datablocks );
+use COD::CIF::Tags::Print qw( print_cif );
+
+my $data_block_single = new_datablock( 'single' );
+my $data_block_loop = new_datablock( 'loop' );
+
+set_tag( $data_block_single, '_data_name', 'value' );
+set_loop_tag( $data_block_loop, '_data_name', undef, [ 1, 2, 3 ] );
+
+merge_datablocks( $data_block_single, $data_block_loop, { override_all => 1 } );
+print_cif( $data_block_loop );
+
+END_SCRIPT
