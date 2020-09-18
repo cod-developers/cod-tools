13832f0d9f0bc213f156d0c1fa100843f17c200f
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2020-09-18 14:24:59 +0000

    trunk/ Extending test shtests/cif_diff_001.sh.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8531 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/cif_diff_001.sh
+++ tests/shtests/cif_diff_001.sh
@@ -11,3 +11,7 @@ ${INPUT_SCRIPT} --compare-numeric _tag \
 
 ${INPUT_SCRIPT} --compare-numeric _tag \
     <(echo 'data_A2 _tag 123(4)') <(echo 'data_B2 _tag 120(4)')
+
+${INPUT_SCRIPT} --compare-numeric _tag \
+    <(echo 'data_A3 loop_ _tag 10(2) 10(2) 12(3) 10(1)') \
+    <(echo 'data_B3 loop_ _tag 10(2) 11(2) 12(3) 20(1)')
