c355a59408815afb2711472333ea8ccb510ba944
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2020-10-21 20:02:55 +0000

    cod-tools/ (antanas@kurmis) Fixed: updated the 'cif_CODify' script usage example and added proper handling of situations when the input CIF file is not provided.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8557 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/cif_CODify_003.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_SCRIPT=scripts/cif_CODify
+INPUT_CIF=""
+#END DEPEND--------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#**
+#* Tests the way the 'cif_CODify' script behaves when the input CIF file
+#* is not provided.
+#**
+
+set -ue
+
+PATH=.:${PATH}
+
+${INPUT_SCRIPT} ${INPUT_CIF}
