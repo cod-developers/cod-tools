96def36353f769b5ad7e9fb7e7e60538f6838d9f
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2020-11-06 02:18:13 +0000

    trunk/ (antanas@kurmis) Changed: slightly reorganised the test files.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8568 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/cif2json_003.sh
+++ tests/shtests/cif2json_003.sh
@@ -6,7 +6,7 @@ INPUT_CIF2JSON=scripts/cif2json
 INPUT_CIF_CORRECT_TAGS=scripts/cif_correct_tags
 INPUT_CIF2COD=scripts/cif2cod
 INPUT_CIF1=tests/inputs/2000000-trimmed.cif
-INPUT_CIF2=tests/inputs/4303187-multiple-underscores.cif
+INPUT_CIF2=tests/inputs/cif2json/4303187-multiple-underscores.cif
 
 #END DEPEND--------------------------------------------------------------------
 
--- tests/shtests/cif2json_004.sh
+++ tests/shtests/cif2json_004.sh
@@ -5,7 +5,7 @@
 INPUT_CIF2JSON=scripts/cif2json
 INPUT_CIF_CORRECT_TAGS=scripts/cif_correct_tags
 INPUT_JSON2CIF=scripts/json2cif
-INPUT_CIF=tests/inputs/4303187-multiple-underscores.cif
+INPUT_CIF=tests/inputs/cif2json/4303187-multiple-underscores.cif
 
 #END DEPEND--------------------------------------------------------------------
 
