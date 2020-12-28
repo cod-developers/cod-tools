55848962d51de3ec09289110b73b24ff77e55352
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2020-12-28 21:01:23 +0000

    trunk/ (antanas@kurmis) Removed: dependencies for Fedora-31 since the OS is no longer supported.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8640 04be6746-3802-0410-999d-98508da1e98c

--- dependencies/Fedora-31/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-sudo dnf install -y cmake
-sudo dnf install -y libnauty-devel
-sudo dnf install -y perl-Module-ScanDeps
-sudo dnf install -y perl-Parse-Yapp bison
-sudo dnf install -y python3-devel
-sudo dnf install -y swig
