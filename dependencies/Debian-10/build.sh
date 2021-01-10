e80f51288654d8331f3a8270d8337a605473b0fc
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2021-01-10 04:34:43 +0000

    trunk/ (antanas@kurmis) Added: dependencies for Debian 10.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8642 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Debian-10/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+    bison \
+    cmake \
+    gcc \
+    libmodule-scandeps-perl \
+    libnauty2-dev \
+    libparse-yapp-perl \
+    make \
+    perl \
+    python-dev \
+    python-setuptools \
+    python3-dev \
+    python3-setuptools \
+    swig \
+;
