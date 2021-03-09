4ee83f553f4effa298b427e89a0038c22bc24059
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2021-03-09 21:26:19 +0000

    trunk/src/lib/perl5/COD/CIF/DDL/DDLm/ (antanas@kurmis) Changed: slightly refactored the COD::CIF::DDL:DDLm::Import module for readability.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@8656 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/CIF/DDL/DDLm/Import.pm
+++ src/lib/perl5/COD/CIF/DDL/DDLm/Import.pm
@@ -74,9 +74,9 @@ sub resolve_dic_imports
     my ( $dic_block, $options ) = @_;
 
     my $import_path        = $options->{'import_path'};
+    my $importing_file     = $options->{'importing_file'};
     my $parser_options     = $options->{'parser_options'};
     my $die_on_error_level = $options->{'die_on_error_level'};
-    my $importing_file     = $options->{'importing_file'};
 
     my $import_dependencies = get_imported_files(
             $dic_block,
@@ -199,9 +199,9 @@ sub get_imported_files
 {
     my ( $dic_block, $options ) = @_;
 
-    my $import_path        = $options->{'import_path'};
-    my $parser_options     = $options->{'parser_options'};
-    my $importing_file     = $options->{'importing_file'};
+    my $import_path    = $options->{'import_path'};
+    my $importing_file = $options->{'importing_file'};
+    my $parser_options = $options->{'parser_options'};
 
     my $imported_files = resolve_import_dependencies(
         {
