81dc6b0dfff0c891b518584362c5bca64d9231d5
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2021-12-08 12:33:37 +0000

    branches/feature/andrius-MarkDisorder-module/ Adding a test for new assembly name generation.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/andrius-MarkDisorder-module@9031 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/get_new_assembly_names_001.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULE=src/lib/perl5/COD/CIF/Data/MarkDisorder.pm
+#END DEPEND--------------------------------------------------------------------
+
+perl <<'END_SCRIPT'
+
+use strict;
+use warnings;
+use COD::CIF::Data::MarkDisorder;
+use Data::Dumper;
+
+my @cases = map { [ $_ ] } qw( A Z ID-001 TEST-NAME _ _0 );
+unshift @cases, [];
+
+for (@cases) {
+    print Dumper
+        [ COD::CIF::Data::MarkDisorder::get_new_assembly_names( $_ ) ];
+}
+
+END_SCRIPT
