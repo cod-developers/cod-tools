21bb20e64c8ec635c136a41e3d0e787f0aae29c6
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2021-12-09 08:40:03 +0000

    branches/feature/andrius-MarkDisorder-module/ Adding yet another test case in tests/shtests/get_new_assembly_names_001.sh.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/andrius-MarkDisorder-module@9038 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/get_new_assembly_names_001.sh
+++ tests/shtests/get_new_assembly_names_001.sh
@@ -14,6 +14,7 @@ use Data::Dumper;
 my @cases = map { [ $_ ] } qw( A Z ID-001 TEST-NAME _ _0 );
 unshift @cases, [];
 push @cases, [ 9, 10 ];
+push @cases, [ 'AA', 'Z' ];
 
 for (@cases) {
     print Dumper
