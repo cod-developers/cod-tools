32614a1aa1f96b029c1aeaf72d2dd15a1303e89b
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2021-12-13 11:01:07 +0000

    branches/feature/ (antanas@echidna) Changed: updated comments in a test case.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/andrius-MarkDisorder-module@9049 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/generate_additional_assembly_names_001.sh
+++ tests/shtests/generate_additional_assembly_names_001.sh
@@ -5,8 +5,8 @@ INPUT_MODULE=src/lib/perl5/COD/CIF/Data/MarkDisorder.pm
 #END DEPEND--------------------------------------------------------------------
 #**
 #* Unit test for the COD::CIF::Data::MarkDisorder::generate_additional_assembly_names()
-#* subroutine. Tests the way sequential numeric values with an different number
-#* of digits are handled.
+#* subroutine. Tests the way additional assembly codes are generated based on
+#* the existing ones. 
 #**
 
 perl <<'END_SCRIPT'
@@ -14,10 +14,10 @@ perl <<'END_SCRIPT'
 use strict;
 use warnings;
 use COD::CIF::Data::MarkDisorder;
-use Data::Dumper;
 
 my @cases = map { [ $_ ] } qw( . A Z ID-001 TEST-NAME _ _0 );
 unshift @cases, [];
+# Sequential numeric values with an different number of digits.
 push @cases, [ 9, 10 ];
 push @cases, [ 'AA', 'Z' ];
 push @cases, [  '.', 'A' ];
