07f9195c8f42485b0a4a5405a6004523045a78d5
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2023-08-22 12:39:40 +0000

    trunk/ (antanas@vilkas) Added a dependency installer for Ubuntu 22.04.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@9622 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Ubuntu-22.04/run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+    curl \
+    gawk \
+    libclone-perl \
+    libdata-compare-perl \
+    libdate-calc-perl \
+    libdatetime-format-rfc3339-perl \
+    libdbd-mysql-perl \
+    libdbd-sqlite3-perl \
+    libdbi-perl \
+    libdigest-sha-perl \
+    libgraph-perl \
+    libhtml-parser-perl \
+    libjson-perl \
+    liblist-moreutils-perl \
+    libparse-yapp-perl \
+    libtext-unidecode-perl \
+    libwww-curl-perl \
+    libxml-simple-perl \
+    openbabel \
+    perl \
+    python-argparse \
+    libmath-bigint-gmp-perl \
+;
+
+# The 'libmath-bigint-gmp-perl' package greatly improves the speed of
+# BigRat arithmetics.
---
+++ dependencies/Ubuntu-22.04/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# The following packages are necessary to run tests for some
+# components of the 'cod-tools' source tree:
+
+sudo apt-get install -y \
+    libtext-diff-perl
