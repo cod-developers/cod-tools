99acc79651f57716b791bee317fab41983eed3e2
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2023-08-22 11:04:28 +0000

    trunk/ (antanas@vilkas) Added a dependency installer for Debian 12.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@9621 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Debian-12/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+    bison \
+    cmake \
+    gcc \
+    libmodule-scandeps-perl \
+    libnauty2-dev \
+    libparse-yapp-perl \
+    make \
+    perl \
+    python3-dev \
+    python3-setuptools \
+    python-dev-is-python3 \
+    swig \
+;
---
+++ dependencies/Debian-12/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# The following packages are necessary to run tests for some
+# components of the 'cod-tools' source tree:
+
+sudo apt-get install -y \
+    gawk \
+    libtext-diff-perl \
+;
