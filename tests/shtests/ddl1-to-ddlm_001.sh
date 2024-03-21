d5691bc1d29825d55ba54a83765ec62741746176
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2024-03-21 23:41:14 +0000

    Fixed a test case to not fail in other timezones.
    cod-tools/ (antanas@vilkas)
    
    
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@9914 04be6746-3802-0410-999d-98508da1e98c

--- tests/shtests/ddl1-to-ddlm_001.sh
+++ tests/shtests/ddl1-to-ddlm_001.sh
@@ -5,5 +5,5 @@ INPUT_SCRIPT=scripts/ddl1-to-ddlm
 INPUT_DIC=tests/inputs/cif_cod.dic
 #END DEPEND--------------------------------------------------------------------
 
-CURRENT_DATE=$(date +%F)
+CURRENT_DATE=$(date +%F --utc)
 ${INPUT_SCRIPT} ${INPUT_DIC} | perl -lpe "s/${CURRENT_DATE}\$/YYYY-MM-DD/"
