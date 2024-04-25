a4a75e6eb2f99a11c904a08d9ddb584ae607b976
Yaroslav_Rozdobudko <Yaroslav_Rozdobudko@04be6746-3802-0410-999d-98508da1e98c>
2024-04-25 23:19:09 +0000

    Implemented multiedges, and support for loops.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/experiment/yaroslav-polymer-quotient-graphs@9975 04be6746-3802-0410-999d-98508da1e98c

--- src/lib/perl5/COD/GraphCycleBasis.pm
+++ src/lib/perl5/COD/GraphCycleBasis.pm
@@ -7,8 +7,9 @@ use Graph;
 sub new {
     my ($class, %opts) = @_;
     my $self = {
-        graph => Graph->new(%opts),
+        graph => Graph->new(multiedged => 1),
         edge_labels => {},
+        edge_counter => 0,
     };
     bless $self, $class;
     return $self;
@@ -16,14 +17,16 @@ sub new {
 
 sub add_edge_with_label {
     my ($self, $u, $v, $label) = @_;
-    $self->{graph}->add_edge($u, $v);
-    $self->{edge_labels}{$u}{$v} = $label;
-    $self->{edge_labels}{$v}{$u} = $label;
+    my $edge_id = ++$self->{edge_counter};
+    $self->{graph}->add_edge_by_id($u, $v, $edge_id);
+    
+    $self->{edge_labels}{$u}{$v}{$edge_id} = $label;
+    $self->{edge_labels}{$v}{$u}{$edge_id} = $label;
 }
 
 sub get_edge_label {
-    my ($self, $u, $v) = @_;
-    return $self->{edge_labels}{$u}{$v};
+    my ($self, $u, $v, $edge_id) = @_;
+    return $self->{edge_labels}{$u}{$v}{$edge_id};
 }
 
 sub get_cycle_basis {
@@ -35,17 +38,23 @@ sub get_cycle_basis {
     for my $f (keys %$spanning_forest) {
         my $edge = $spanning_forest->{$f};
         if($edge) {
-            my ($source, $target) = @$edge;
-            $tree_edges{$source}{$target} = 1;
+            my ($source, $target, $id) = @$edge;
+            $tree_edges{$source}{$target}{$id} = 1;
         }
     }
     my @cycles;
     my @labels_collection;
     my $length = 0;
-
-    for my $e ($graph->edges) {
-        my ($source, $target) = @$e;
-        unless($tree_edges{$source}{$target}) {
+    my @edges1 = $graph->edges;
+    my $all_edges = get_all_edges_with_ids($self);
+    
+    for my $e (@$all_edges) {
+        my ($source, $target, $id) = @$e;
+        #my @ids = $graph->get_multiedge_ids($source, $target);
+        #my $attributes = $graph->get_edge_attributes($source, $target);
+        #my $attributes1 = $graph->get_edge_attribute_names($source, $target);
+        #my $attributes2 = $graph->get_edge_attribute_values($source, $target);
+        unless($tree_edges{$source}{$target}{$id}) {
             my ($cycle, $labels) = $self->buildFundamentalCycle($e, $spanning_forest);
             push(@cycles, $cycle);
             push(@labels_collection, $labels);
@@ -68,8 +77,11 @@ sub compute_spanning_forest {
 
         while(@queue) {
             my $v = shift(@queue);
-
-            for my $e (sort { $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] } $graph->edges_at($v)) {
+            my $edges_with_ids = edges_at_with_ids($self, $v);
+            my @sorted_edges = sort {
+                $a->[0] cmp $b->[0] || $a->[1] cmp $b->[1] || $a->[2] <=> $b->[2]
+            } @$edges_with_ids;
+            for my $e (@sorted_edges) {
                 my $u = _getOppositeVertex($self, $e, $v);
                 unless(exists $pred{$u}) {
                     $pred{$u} = $e;
@@ -91,26 +103,28 @@ sub compute_spanning_forest {
 
 sub buildFundamentalCycle {
     my ($self, $edge, $spanningForest) = @_;
-    my ($source, $target) = @$edge;
+    my ($source, $target, $id) = @$edge;
     if($source eq $target) {
-        my $weight = $self->{edge_labels}{$source}{$target};
-        my %labels = ($edge=>$weight);
-        return [[$edge], \%labels];
+        my $weight = $self->{edge_labels}{$source}{$target}{$id};
+        my %labels;# = ($edge=>$weight);
+        $labels{$source}{$target}{$id} = $weight;
+        my @edge = [$source, $target, $id];
+        return (\@edge, \%labels);
     }
 
     #my @path1 = ($edge);
-    my $label = $self->{edge_labels}{$source}{$target};
+    my $label = $self->{edge_labels}{$source}{$target}{$id};
     my %path1;# = ($edge => $label);
-    $path1{$source}{$target} = $label;
+    $path1{$source}{$target}{$id} = $label;
     my $current = $source;
 
     while($current ne $target) {
         my $edgeToParent = $spanningForest->{$current};
         last unless defined $edgeToParent;
         my $parent = _getOppositeVertex($self, $edgeToParent, $current);
-        my $label = $self->{edge_labels}{$current}{$parent};
-        my ($source, $target) = @$edgeToParent;
-        $path1{$source}{$target} = $label;
+        my ($source_parent, $target_parent, $id_parent) = @$edgeToParent;
+        my $label = $self->{edge_labels}{$source_parent}{$target_parent}{$id_parent};
+        $path1{$source_parent}{$target_parent}{$id_parent} = $label;
         #push(@path1, $edgeToParent);
         #$path1_set{$edgeToParent} = 1;
         $current = $parent;
@@ -125,22 +139,24 @@ sub buildFundamentalCycle {
             my $edgeToParent = $spanningForest->{$current};
             last unless defined $edgeToParent;
             my $parent = _getOppositeVertex($self, $edgeToParent, $current);
-            my ($source, $target) = @$edgeToParent;
-            if($path1{$source}{$target}) {
-                delete $path1{$source}{$target}; 
+            my ($source_parent, $target_parent, $id_parent) = @$edgeToParent;
+            if(exists $path1{$source_parent} and $path1{$source_parent}{$target_parent} and $path1{$source_parent}{$target_parent}{$id_parent}) {
+                delete $path1{$source_parent}{$target_parent}{$id_parent}; 
             } else {
                 push(@path2, $edgeToParent);
-                my $label = $self->{edge_labels}{$current}{$parent};
-                $path2Weight{$source}{$target} = $label;
+                my $label = $self->{edge_labels}{$source_parent}{$target_parent}{$id_parent};
+                $path2Weight{$source_parent}{$target_parent}{$id_parent} = $label;
             }
             $current = $parent;
         }
     }
-    for my $source (keys %path1) {
-        for my $target (keys %{ $path1{$source}}) {
-            unshift(@path2, [$source, $target]);
-            my $label = $self->{edge_labels}{$source}{$target};
-            $path2Weight{$source}{$target} = $label;
+    for my $source_path (keys %path1) {
+        for my $target_path (keys %{ $path1{$source_path}}) {
+            for my $id_path (keys %{ $path1{$source_path}{$target_path}}) {
+                unshift(@path2, [$source_path, $target_path, $id_path]);
+                my $label = $self->{edge_labels}{$source_path}{$target_path}{$id_path};
+                $path2Weight{$source_path}{$target_path}{$id_path} = $label;
+            }
         }
         #unshift(@path2, $e);
         #my ($source, $target) = @$e;
@@ -155,6 +171,45 @@ sub _getOppositeVertex {
     my ($self, $edge, $vertex) = @_;
     my ($source, $target) = @$edge;
     return $source eq $vertex ? $target : $source;
+}
+
+sub get_all_edges_with_ids {
+    my $self = shift;
+    my $graph = $self->{graph};
+    my @all_edges_with_ids;
+    my %added_edges;
+
+    for my $edge ($graph->edges) {
+        my ($source, $target) = @$edge;
+        my @ids = $graph->get_multiedge_ids($source, $target);
+
+        for my $id (@ids) {
+            unless(exists $added_edges{$source}{$target}{$id}) {
+                push(@all_edges_with_ids, [$source, $target, $id]);
+                $added_edges{$source}{$target}{$id} = 1;
+            }
+        }
+    }
+    return \@all_edges_with_ids;
+}
+sub edges_at_with_ids {
+    my ($self, $v) = @_;
+    my $graph = $self->{graph};
+    my @all_edges_with_ids;
+    my %added_edges;
+
+    for my $edge ($graph->edges_at($v)) {
+        my ($source, $target) = @$edge;
+        my @ids = $graph->get_multiedge_ids($source, $target);
+
+        for my $id (@ids) {
+            unless(exists $added_edges{$source}{$target}{$id}) {
+                push(@all_edges_with_ids, [$source, $target, $id]);
+                $added_edges{$source}{$target}{$id} = 1;
+            }
+        }
+    }
+    return \@all_edges_with_ids;
 }
 
 sub add_vertex {
