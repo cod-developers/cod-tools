2f45d467734f6ed919091e917e4d35ad2be3a3fa
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2024-04-29 10:56:59 +0000

    Confirmed dependencies for Ubuntu 24.04.
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@10004 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Ubuntu-24.04/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+    autoconf \
+    bison \
+    cmake \
+    gcc \
+    libmodule-scandeps-perl \
+    libnauty-dev \
+    libparse-yapp-perl \
+    libtool \
+    make \
+    perl \
+    python3-dev \
+    python3-setuptools \
+    swig \
+;
