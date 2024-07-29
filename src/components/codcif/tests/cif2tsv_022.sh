d93ce57de6a3300180f497155a26b2d0a772ce09
saulius <saulius@04be6746-3802-0410-999d-98508da1e98c>
2024-07-29 09:11:52 +0000

    Adding two tests to see how multiple file reading handled.
    branches/feature/saulius-cifvalues-cif2csv-use-of-ascii-delimiters/src/components/ (saulius@pterodaktilis)
    
    
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/saulius-cifvalues-cif2csv-use-of-ascii-delimiters@10144 04be6746-3802-0410-999d-98508da1e98c

---
+++ src/components/codcif/tests/cif2tsv_022.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ue
+
+./cif2tsv \
+    --header \
+    --datablock-name \
+    --filename \
+    --tag "_cell_length_a,_cell_length_b,_cell_length_c,_chemical_formula_sum" \
+    inputs/2000000.cif \
+    inputs/1004001-disorder.cif \
+    inputs/1508696-long-text-field.cif \
+    inputs/2000000.cif \
+    inputs/4067640.cif \
+    inputs/4318422.cif \
+    inputs/4319975.cif \
+    inputs/7052868.cif \
+    inputs/9007991.cif \
+    #
