d412261acf6d9640c436a3ecbb3f7f8bfda50ee4
andrius <andrius@04be6746-3802-0410-999d-98508da1e98c>
2026-01-09 14:09:21 +0000

    branches/feature/andrius-fix-chemistry-opensmiles-conversion/ Fix an issue with sparse atoms in the output of Chemistry::OpenSMILES.
    git-svn-id: svn://www.crystallography.net/cod-tools/branches/feature/andrius-fix-chemistry-opensmiles-conversion@10740 04be6746-3802-0410-999d-98508da1e98c

---
+++ tests/shtests/neighbour_list_from_chemistry_opensmiles_002.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#BEGIN DEPEND------------------------------------------------------------------
+INPUT_MODULE=src/lib/perl5/COD/AtomNeighbours.pm
+#END DEPEND--------------------------------------------------------------------
+
+IMPORT_MODULE=$(\
+    echo ${INPUT_MODULE} | \
+    perl -pe "s|^src/lib/perl5/||; s/[.]pm$//; s|/|::|g;" \
+)
+
+perl -M"${IMPORT_MODULE} qw( neighbour_list_from_chemistry_opensmiles )" \
+<<'END_SCRIPT'
+#------------------------------------------------------------------------------
+#$Author$
+#$Date$ 
+#$Revision$
+#$URL$
+#------------------------------------------------------------------------------
+#*
+#* Unit test for the COD::AtomNeighbours::neighbour_list_from_chemistry_opensmiles() subroutine.
+#**
+
+use strict;
+use warnings;
+
+# use COD::AtomNeighbours qw( neighbour_list_from_chemistry_opensmiles );
+use Chemistry::OpenSMILES::Parser;
+use Data::Dumper;
+
+$Data::Dumper::Sortkeys = 1;
+
+my $parser = Chemistry::OpenSMILES::Parser->new;
+my @moieties = $parser->parse( 'C.C' );
+
+my $neighbour_list = neighbour_list_from_chemistry_opensmiles( $moieties[0] );
+print Dumper $neighbour_list->{atoms};
+
+END_SCRIPT
