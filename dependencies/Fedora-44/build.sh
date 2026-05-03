af95f5aa3b4d97086a8443002abb1e9092c7b09f
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2026-05-03 17:40:03 +0000

    Confirm dependencies for Fedora-44
    trunk/ (antanas@vilkas)
    
    
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@10798 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Fedora-44/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo dnf install -y \
+    bison \
+    cmake \
+    libnauty-devel \
+    make \
+    patch \
+    perl-devel \
+    perl-Module-ScanDeps \
+    perl-Parse-Yapp \
+    python3-devel \
+    python3-setuptools \
+    redhat-rpm-config \
+    swig \
+;
---
+++ dependencies/Fedora-44/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo dnf install -y \
+    perl-Test-More-UTF8 \
+    perl-Text-Diff \
+    perl-FindBin \
+;
