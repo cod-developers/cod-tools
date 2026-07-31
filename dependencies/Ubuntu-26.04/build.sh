025796a5544afc7d1f2a5092080dac84bd52da73
antanas <antanas@04be6746-3802-0410-999d-98508da1e98c>
2026-07-31 09:46:38 +0000

    Add dependency installer for Ubuntu 26.04
    git-svn-id: svn://www.crystallography.net/cod-tools/trunk@10924 04be6746-3802-0410-999d-98508da1e98c

---
+++ dependencies/Ubuntu-26.04/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt-get install -y \
+    autoconf \
+    bison \
+    cmake \
+    gcc \
+    libcrypt-dev \
+    libmodule-scandeps-perl \
+    libnauty-dev \
+    libparse-yapp-perl \
+    libtool \
+    make \
+    perl \
+    python3-dev \
+    python3-setuptools \
+    swig \
+;
